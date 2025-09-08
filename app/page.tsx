b4b32f519020051e5ae54dccb3f2752bc8eba1e4
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-09-08 12:53:14 +0530

    Fix formatting: add space in BlueGlow component usage for consistency

--- app/page.tsx
+++ app/page.tsx
@@ -8,9 +8,10 @@ export default function Home() {
   return (
     <>
       <div className="flex flex-col items-center justify-center">
-        <BlueGlow/>
+        <BlueGlow />
         <MeshVector />
         <NavBar />
+        <p className="text-lg border-1 rounded-4xl p-2">Bring your business to the best scale</p>
       </div>
     </>
   );
