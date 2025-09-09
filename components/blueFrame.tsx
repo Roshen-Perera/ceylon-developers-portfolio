1a7031ef99b8705c218184d715175a1a904796fd
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-09-09 17:56:50 +0530

    Add blueFrame component with SVG graphics for enhanced visual design

---
+++ components/blueFrame.tsx
@@ -0,0 +1,149 @@
+import React from 'react'
+
+const blueFrame = () => {
+  return (
+    <div>
+      <svg
+        width="357"
+        height="10"
+        viewBox="0 0 357 10"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g filter="url(#filter0_f_1085_1326)">
+          <line
+            x1="110.608"
+            y1="5"
+            x2="246.608"
+            y2="5"
+            stroke="url(#paint0_linear_1085_1326)"
+            stroke-width="2"
+          />
+        </g>
+        <g filter="url(#filter1_f_1085_1326)">
+          <line
+            x1="4.6084"
+            y1="5"
+            x2="352.608"
+            y2="5"
+            stroke="url(#paint1_linear_1085_1326)"
+            stroke-width="2"
+          />
+        </g>
+        <g filter="url(#filter2_f_1085_1326)">
+          <line
+            x1="110.608"
+            y1="5"
+            x2="246.608"
+            y2="5"
+            stroke="url(#paint2_linear_1085_1326)"
+            stroke-width="2"
+          />
+        </g>
+        <defs>
+          <filter
+            id="filter0_f_1085_1326"
+            x="106.608"
+            y="0"
+            width="144"
+            height="10"
+            filterUnits="userSpaceOnUse"
+            colorInterpolationFilters="sRGB"
+          >
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="BackgroundImageFix"
+              result="shape"
+            />
+            <feGaussianBlur
+              stdDeviation="2"
+              result="effect1_foregroundBlur_1085_1326"
+            />
+          </filter>
+          <filter
+            id="filter1_f_1085_1326"
+            x="0.608398"
+            y="0"
+            width="356"
+            height="10"
+            filterUnits="userSpaceOnUse"
+            colorInterpolationFilters="sRGB"
+          >
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="BackgroundImageFix"
+              result="shape"
+            />
+            <feGaussianBlur
+              stdDeviation="2"
+              result="effect1_foregroundBlur_1085_1326"
+            />
+          </filter>
+          <filter
+            id="filter2_f_1085_1326"
+            x="106.608"
+            y="0"
+            width="144"
+            height="10"
+            filterUnits="userSpaceOnUse"
+            colorInterpolationFilters="sRGB"
+          >
+            <feFlood flood-opacity="0" result="BackgroundImageFix" />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="BackgroundImageFix"
+              result="shape"
+            />
+            <feGaussianBlur
+              stdDeviation="2"
+              result="effect1_foregroundBlur_1085_1326"
+            />
+          </filter>
+          <linearGradient
+            id="paint0_linear_1085_1326"
+            x1="110.608"
+            y1="6.5"
+            x2="246.608"
+            y2="6.5"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#17CDCA" stopOpacity="0" />
+            <stop offset="0.5" stopColor="#17CDCA" />
+            <stop offset="1" stopColor="#17CDCA" stopOpacity="0" />
+          </linearGradient>
+          <linearGradient
+            id="paint1_linear_1085_1326"
+            x1="4.6084"
+            y1="6.5"
+            x2="352.608"
+            y2="6.5"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#17CDCA" stopOpacity="0" />
+            <stop offset="0.5" stopColor="#17CDCA" />
+            <stop offset="1" stopColor="#17CDCA" stopOpacity="0" />
+          </linearGradient>
+          <linearGradient
+            id="paint2_linear_1085_1326"
+            x1="110.608"
+            y1="6.5"
+            x2="246.608"
+            y2="6.5"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#17CDCA" stopOpacity="0" />
+            <stop offset="0.5" stopColor="#17CDCA" />
+            <stop offset="1" stopColor="#17CDCA" stopOpacity="0" />
+          </linearGradient>
+        </defs>
+      </svg>
+    </div>
+  );
+}
+
+export default blueFrame
