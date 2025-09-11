571c95f141eaee47183cae266fc9251a74f0a178
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-09-11 14:29:38 +0530

    Remove duplicate ServiceCard components for cleaner code and improved maintainability

--- components/serviceCard.tsx
+++ components/serviceCard.tsx
@@ -14,42 +14,6 @@ const ServiceCard = () => {
           we live in a digital age.
         </p>
       </div>
-      <div className="justify border-1 rounded-lg p-3">
-        <Image src={"/assets/icons/web.gif"} alt={""} width={40} height={40} />
-        <p className="text-xl font-semibold">Web Design & Development</p>
-        <p className="text-lg">
-          A Website is an extension of yourself and we can help you to express
-          it properly. Your website is your number one marketing asset because
-          we live in a digital age.
-        </p>
-      </div>
-      <div className="justify border-1 rounded-lg p-3">
-        <Image src={"/assets/icons/web.gif"} alt={""} width={40} height={40} />
-        <p className="text-xl font-semibold">Web Design & Development</p>
-        <p className="text-lg">
-          A Website is an extension of yourself and we can help you to express
-          it properly. Your website is your number one marketing asset because
-          we live in a digital age.
-        </p>
-      </div>
-      <div className="justify border-1 rounded-lg p-3">
-        <Image src={"/assets/icons/web.gif"} alt={""} width={40} height={40} />
-        <p className="text-xl font-semibold">Web Design & Development</p>
-        <p className="text-lg">
-          A Website is an extension of yourself and we can help you to express
-          it properly. Your website is your number one marketing asset because
-          we live in a digital age.
-        </p>
-      </div>
-      <div className="justify border-1 rounded-lg p-3">
-        <Image src={"/assets/icons/web.gif"} alt={""} width={40} height={40} />
-        <p className="text-xl font-semibold">Web Design & Development</p>
-        <p className="text-lg">
-          A Website is an extension of yourself and we can help you to express
-          it properly. Your website is your number one marketing asset because
-          we live in a digital age.
-        </p>
-      </div>
     </>
   );
 };
