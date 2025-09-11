73adfcf49797a6fb02076ed4b7d6c8693ec24c02
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-09-12 01:20:26 +0530

    Remove unnecessary 'relative' class from paragraph for cleaner styling

--- app/page.tsx
+++ app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         <BlueGlow />
         <MeshVector />
         <NavBar />
-        <p className="text-lg mt-18 border-1 rounded-4xl p-2 relative">
+        <p className="text-lg mt-18 border-1 rounded-4xl p-2 ">
           Bring your business to the best scale
         </p>
         <div className="flex flex-col mt-8 z-20 items-center">
