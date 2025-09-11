7c901ca3dc2eaa7c075f5ce82c42b52cdbedf3f0
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-09-11 14:28:14 +0530

    Refactor ServiceCard component to improve structure and maintainability

--- components/serviceCard.tsx
+++ components/serviceCard.tsx
@@ -1,12 +1,57 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 const ServiceCard = () => {
-  return (
-    <div>
-        <Image src={"/assets/icons/web.gif"} alt={''} width={50} height={50}  />
-    </div>
-  )
-}
 
-export default ServiceCard
+  return (
+    <>
+      <div className="justify border-1 rounded-lg p-3">
+        <Image src={"/assets/icons/web.gif"} alt={""} width={40} height={40} />
+        <p className="text-xl font-semibold">Web Design & Development</p>
+        <p className="text-lg">
+          A Website is an extension of yourself and we can help you to express
+          it properly. Your website is your number one marketing asset because
+          we live in a digital age.
+        </p>
+      </div>
+      <div className="justify border-1 rounded-lg p-3">
+        <Image src={"/assets/icons/web.gif"} alt={""} width={40} height={40} />
+        <p className="text-xl font-semibold">Web Design & Development</p>
+        <p className="text-lg">
+          A Website is an extension of yourself and we can help you to express
+          it properly. Your website is your number one marketing asset because
+          we live in a digital age.
+        </p>
+      </div>
+      <div className="justify border-1 rounded-lg p-3">
+        <Image src={"/assets/icons/web.gif"} alt={""} width={40} height={40} />
+        <p className="text-xl font-semibold">Web Design & Development</p>
+        <p className="text-lg">
+          A Website is an extension of yourself and we can help you to express
+          it properly. Your website is your number one marketing asset because
+          we live in a digital age.
+        </p>
+      </div>
+      <div className="justify border-1 rounded-lg p-3">
+        <Image src={"/assets/icons/web.gif"} alt={""} width={40} height={40} />
+        <p className="text-xl font-semibold">Web Design & Development</p>
+        <p className="text-lg">
+          A Website is an extension of yourself and we can help you to express
+          it properly. Your website is your number one marketing asset because
+          we live in a digital age.
+        </p>
+      </div>
+      <div className="justify border-1 rounded-lg p-3">
+        <Image src={"/assets/icons/web.gif"} alt={""} width={40} height={40} />
+        <p className="text-xl font-semibold">Web Design & Development</p>
+        <p className="text-lg">
+          A Website is an extension of yourself and we can help you to express
+          it properly. Your website is your number one marketing asset because
+          we live in a digital age.
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default ServiceCard;
