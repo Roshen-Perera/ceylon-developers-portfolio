3bf1dd62383f80bfccef4f6cdcc8fb493f9ac965
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-09-16 02:11:41 +0530

    created approches

--- app/page.tsx
+++ app/page.tsx
@@ -138,7 +138,7 @@ export default function Home() {
           </div>
         </div>
         <div className="flex flex-col items-center justify-center mt-12">
-          <div className="text-center">
+          <div className="text-center mb-8">
             <p className="text-5xl font-medium">
               Our <span className="font-kaushan">design</span> and
               <br />
@@ -148,7 +148,7 @@ export default function Home() {
           <div className="grid grid-cols-4 p-8">
             <div className="p-3 shadow-[0px_0px_15px_rgba(23,205,202,0.47)] rounded-lg">
               <div className="flex flex-col p-5 gradient-border rounded-2xl gap-4 ">
-                <p className="text-lg font-semibold">UX Driven Engineering</p>
+                <p className="text-lg font-bold">UX Driven Engineering</p>
                 <p className="text-lg text-[#585858]">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. sit
                   Lorem ipsum dolor sit amet, elit consectetur adipiscing elit.{" "}
@@ -158,7 +158,9 @@ export default function Home() {
             <div></div>
             <div className="p-3 shadow-[0px_0px_15px_rgba(23,205,202,0.47)] rounded-lg">
               <div className="flex flex-col p-5 gradient-border rounded-2xl gap-4 ">
-                <p className="text-lg font-semibold">UX Driven Engineering</p>
+                <p className="text-lg font-bold">
+                  Proven Experience and Expertise
+                </p>
                 <p className="text-lg text-[#585858]">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. sit
                   Lorem ipsum dolor sit amet, elit consectetur adipiscing elit.{" "}
@@ -171,7 +173,9 @@ export default function Home() {
             <div></div>
             <div className="p-3 shadow-[0px_0px_15px_rgba(23,205,202,0.47)] rounded-lg">
               <div className="flex flex-col p-5 gradient-border rounded-2xl gap-4 ">
-                <p className="text-lg font-semibold">UX Driven Engineering</p>
+                <p className="text-lg font-bold">
+                  Developing Shared Understanding
+                </p>
                 <p className="text-lg text-[#585858]">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. sit
                   Lorem ipsum dolor sit amet, elit consectetur adipiscing elit.{" "}
@@ -181,7 +185,7 @@ export default function Home() {
             <div></div>
             <div className="p-3 shadow-[0px_0px_15px_rgba(23,205,202,0.47)] rounded-lg">
               <div className="flex flex-col p-5 gradient-border rounded-2xl gap-4 ">
-                <p className="text-lg font-semibold">UX Driven Engineering</p>
+                <p className="text-lg font-bold">Quality Assurance & Testing</p>
                 <p className="text-lg text-[#585858]">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. sit
                   Lorem ipsum dolor sit amet, elit consectetur adipiscing elit.{" "}
@@ -192,7 +196,9 @@ export default function Home() {
           <div className="grid grid-cols-4 p-8">
             <div className="p-3 shadow-[0px_0px_15px_rgba(23,205,202,0.47)] rounded-lg">
               <div className="flex flex-col p-5 gradient-border rounded-2xl gap-4 ">
-                <p className="text-lg font-semibold">UX Driven Engineering</p>
+                <p className="text-lg font-bold">
+                  Security & Intellectual Property
+                </p>
                 <p className="text-lg text-[#585858]">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. sit
                   Lorem ipsum dolor sit amet, elit consectetur adipiscing elit.{" "}
@@ -202,7 +208,7 @@ export default function Home() {
             <div></div>
             <div className="p-3 shadow-[0px_0px_15px_rgba(23,205,202,0.47)] rounded-lg">
               <div className="flex flex-col p-5 gradient-border rounded-2xl gap-4 ">
-                <p className="text-lg font-semibold">UX Driven Engineering</p>
+                <p className="text-lg font-bold">Code Reviews</p>
                 <p className="text-lg text-[#585858]">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. sit
                   Lorem ipsum dolor sit amet, elit consectetur adipiscing elit.{" "}
