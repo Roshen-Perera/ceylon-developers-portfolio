757311a1ed38902672baf266322a821e41c4c856
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-09-17 17:43:03 +0530

    increased the font size of the text of hero section

--- app/page.tsx
+++ app/page.tsx
@@ -16,7 +16,7 @@ export default function Home() {
         <MeshVector />
         <NavBar />
 
-        <p className="text-lg mt-18 border-1 rounded-4xl p-2">
+        <p className="text-[22px] mt-18 border-1 rounded-4xl p-2">
           Bring your business to the best scale
         </p>
         <div className="flex flex-col mt-8 z-20 items-center">
@@ -26,13 +26,13 @@ export default function Home() {
           <p className="font-semibold text-8xl items-center">Excellence</p>
         </div>
         <div className="text-lg mt-8 max-w-2xl flex items-center mx-auto px-4">
-          <p className="text-center">
+          <p className="text-[22px] text-center">
             Pushing the boundaries of what&apos;s possible in the digital realm.
             Join us on this journey into the future of technology.
           </p>
         </div>
-        <div className="mt-8 flex max-w-2xl gap-10 justify-center items-center">
-          <button className="p-3 text-lg bg-[#17CDCA] rounded-2xl">
+        <div className="mt-8 flex max-w-4xl gap-10 justify-center items-center">
+          <button className="text-[22px] p-3 text-lg bg-[#17CDCA] rounded-2xl">
             Learn More
           </button>
           <div className="flex flex-row items-center gap-3">
@@ -43,7 +43,7 @@ export default function Home() {
               height={64} // h-16
               className="rounded-full"
             />
-            <p className="text-gray-800">
+            <p className="text-[22px] text-gray-800">
               Trusted by over+ 20K people in the World
             </p>
           </div>
