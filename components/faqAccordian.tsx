92f174bf9afe4de6ed0aaa3e9519e316a7399d97
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-09-30 18:14:53 +0530

    Removed uneccasry padding

--- components/faqAccordian.tsx
+++ components/faqAccordian.tsx
@@ -24,7 +24,7 @@ export default function FAQAccordion() {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center p-4">
+    <div className="mt-12 flex items-center justify-center p-4">
       <div className="w-full max-w-3xl">
         <div className="space-y-4">
           {faqs.map((faq, index) => (
