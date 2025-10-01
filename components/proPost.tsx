1421052f0e013ed1cbb5041953496f98382c2907
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-01 10:53:45 +0530

    Add heading

--- components/proPost.tsx
+++ components/proPost.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 const ProPost = () => {
   return (
     <>
-      <div className="max-w-5xl"></div>
+      <div className="p-4 max-w-5xl text-7xl text-center">
+        <h1>Ready to <span className='font-kaushan'> Manage </span> your
+            <br />
+        team like a pro?</h1>
+      </div>
     </>
   );
 }
