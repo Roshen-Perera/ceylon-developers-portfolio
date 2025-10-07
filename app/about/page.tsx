98d3328e8d0f8947e7e03faed9fd38be9a14a7e9
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-07 18:12:48 +0530

    Refactor About page component layout for improved structure

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,14 @@
+import BlueGlow from '@/components/blueGlow'
+import NavBar from '@/components/navBar'
 import React from 'react'
 
 const Page = () => {
   return (
     <>
-    
+    <div className="flex flex-col items-center justify-center">
+        <BlueGlow />
+        <NavBar />
+      </div>
     </>
   )
 }
