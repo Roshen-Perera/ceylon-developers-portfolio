5417255afb83d833d28e11cdc4a71b08ff84338c
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-07 11:13:10 +0530

    Remove Services section and associated content from Home component

--- app/page.tsx
+++ app/page.tsx
@@ -25,20 +25,7 @@ export default function Home() {
         <NavBar />
         <HeroSection/>
       </div>
-      <div className="flex flex-col items-center justify-center mt-12">
-        <p className="text-7xl font-medium">
-          <span className="font-kaushan">Services</span> we offer{" "}
-        </p>
-        <div className="text-[22px] mt-8 max-w-2xl flex items-center mx-auto px-4">
-          <p className="text-center">
-            Pushing the boundaries of what&apos;s possible in the digital realm.
-            Join us on this journey into the future of technology.
-          </p>
-        </div>
-        <div className="mt-8">
-          <ServiceCarousel />
-        </div>
-      </div>
+      
 
       <div className="flex flex-col items-center justify-center mt-12">
         {/* <MeshVector /> */}
