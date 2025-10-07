27509805b732d356a1faaef9d2c96aa418e3dc54
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-07 18:24:27 +0530

    Refactor About page layout for improved structure and readability

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -5,12 +5,26 @@ import React from 'react'
 const Page = () => {
   return (
     <>
-    <div className="flex flex-col items-center justify-center">
+      <div className="flex flex-col items-center justify-center">
         <BlueGlow />
         <NavBar />
+        <div className='mt-15'>
+          <div className="flex flex-col mt-8 z-20 items-center">
+            <p className="font-semibold text-8xl ">
+              Crafting <span className="text-[#17CDCA]">Digital </span>
+            </p>
+            <p className="font-semibold text-8xl items-center">Excellence</p>
+          </div>
+          <div className="text-lg mt-8 max-w-2xl flex items-center mx-auto px-4">
+            <p className="text-[22px] text-center">
+              Pushing the boundaries of what&apos;s possible in the digital
+              realm. Join us on this journey into the future of technology.
+            </p>
+          </div>
+        </div>
       </div>
     </>
-  )
+  );
 }
 
 export default Page
