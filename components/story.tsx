44650a28f2026070bcc4c8717601424f74a629e0
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-07 18:38:07 +0530

    Refactor Story component layout to remove duplicate image rendering

--- components/story.tsx
+++ components/story.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 
 const Story = () => {
   return (
     <div className="flex flex-col items-center justify-center mt-12">
       <div className="flex flex-row items-center gap-12">
+        <div className="flex-shrink-0">
+          <Image
+            src={"/assets/images/storyContainer.png"}
+            alt={"today's activity dashboard"}
+            width={686}
+            height={487}
+            style={{ opacity: 1 }}
+          />
+        </div>
         <div className="flex flex-col gap-6 flex-1 max-w-lg">
           <p className="text-7xl font-medium">
             Our <span className="font-kaushan">Story</span>
@@ -49,18 +58,9 @@ const Story = () => {
             </div>
           </div>
         </div>
-        <div className="flex-shrink-0">
-          <Image
-            src={"/assets/images/storyContainer.png"}
-            alt={"today's activity dashboard"}
-            width={686}
-            height={487}
-            style={{ opacity: 1 }}
-          />
-        </div>
       </div>
     </div>
   );
-}
+};
 
-export default Story
+export default Story;
