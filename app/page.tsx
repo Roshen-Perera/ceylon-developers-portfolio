ee1c35df932f67a39b681ead59551321fd4945ed
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-07 13:15:18 +0530

    Add Reviews component to Home page

--- app/page.tsx
+++ app/page.tsx
@@ -38,6 +38,7 @@ export default function Home() {
       <div className="flex flex-col items-center justify-center mt-12">
         <TechStack />
       </div>
+      <Reviews/>
       <div className="flex flex-col items-center justify-center mt-12">
         <h1 className="text-7xl font-medium text-center">
           How
