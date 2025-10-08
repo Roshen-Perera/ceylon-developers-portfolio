56d9d46bbf36cd4677787a225dab0d4935bf9778
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-08 11:19:30 +0530

    Update WhyChoose component heading and margin for improved presentation

--- components/whyChoose.tsx
+++ components/whyChoose.tsx
@@ -2,10 +2,10 @@ import React from "react";
 
 const WhyChoose = () => {
   return (
-    <div className="flex flex-col items-center justify-center mt-12">
+    <div className="flex flex-col items-center justify-center mt-20">
       <h1 className="text-7xl font-medium text-center">
         Why Choose
-        <span className="font-kaushan"> Ceylon Developers</span>
+        <span className="font-kaushan"> Ceylon Developers ?</span>
       </h1>
     </div>
   );
