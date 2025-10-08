84fdc4d75a0a0a86ecb57434624f7d0e24064cb4
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-08 11:24:16 +0530

    Add missing grid structure and image to WhyChoose component for improved layout

--- components/whyChoose.tsx
+++ components/whyChoose.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 
 const WhyChoose = () => {
@@ -7,6 +8,12 @@ const WhyChoose = () => {
         Why Choose
         <span className="font-kaushan"> Ceylon Developers ?</span>
       </h1>
+      <div className="grid grid-cols-2 gap-8 mt-12 max-w-6xl px-4">
+        <div className="flex flex-col items-center">
+          <Image src="assets/icons/document.png" alt="Document" width={100} height={100} />
+        </div>
+      </div>
+      <div className="grid grid-cols-2 gap-8 mt-12 max-w-6xl px-4"></div>
     </div>
   );
 };
