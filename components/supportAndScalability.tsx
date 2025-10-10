03fbdfbff721efc08db1956976ef742a5c2bc701
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-10 18:04:50 +0530

    Add SupportAndScalability component: initial implementation for support and scalability features

---
+++ components/supportAndScalability.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const SupportAndScalability = () => {
+  return (
+    <div className="flex items-start gap-8 p-6 bg-white max-w-md">
+      <div className="flex-1">
+        <div className="text-[30px] mb-2">24/7</div>
+        <h3 className="text-[26px] font-bold  mb-2">Support</h3>
+        <p className="text-[22px]">
+          Always available assistance for customer inquiries anytime.
+        </p>
+      </div>
+      <div className="w-px h-24 bg-gray-300"></div>
+      <div className="flex-1">
+        <div className="text-[30px] font-bold mb-2">100%</div>
+        <h3 className="text-[26px] font-bold mb-2">Scalability</h3>
+        <p className="text-[22px]">
+          Effortlessly grows with increasing demands and needs.
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default SupportAndScalability
