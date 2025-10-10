2b009e14bde88bb3c6e804b38fdc2525d6393f10
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-10 17:56:42 +0530

    Refactor Features component: add margin-top to improve spacing

--- components/features.tsx
+++ components/features.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Features = () => {
   return (
-    <div>
+    <div className='mt-8'>
       <h3 className="text-[30px]">Our work areas:</h3>
       <svg
         width="273"
