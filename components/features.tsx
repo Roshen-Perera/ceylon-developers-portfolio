291b901a14d501c86616207b4df53ed73773f322
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-10 17:55:30 +0530

    Add Features component: initial implementation for displaying work areas

---
+++ components/features.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Features = () => {
+  return (
+    <div>
+      <h3 className='text-[30px]'>Our work areas:</h3>
+
+    </div>
+  );
+}
+
+export default Features
