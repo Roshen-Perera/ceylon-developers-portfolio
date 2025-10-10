bfc9a71b2efd278a3c4634d196a7d674a018c80f
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-10 17:03:34 +0530

    Refactor Identity component: enhance structure and styling for improved layout

--- components/identity.tsx
+++ components/identity.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Identity = () => {
   return (
-    <div>
-      
+    <div className="flex flex-col items-center justify-center mt-20">
+      <div className="flex flex-row items-center gap-12">
+        <div className="flex-shrink-0"></div>
+        <div className="flex flex-col gap-6 flex-1 max-w-lg">
+          <p className="text-7xl font-medium">
+            The identity of
+            <br />
+            <span className="font-kaushan"> our brand</span>
+          </p>
+          <p className="text-[22px] text-gray-600">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </p>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Identity
+export default Identity;
