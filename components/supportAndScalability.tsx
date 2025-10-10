35b15bb9fc7fb95c2713a5cdb125ffa7a5947f48
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-10 18:07:11 +0530

    Refactor SupportAndScalability component: enhance styling with padding, border radius, and box shadow

--- components/supportAndScalability.tsx
+++ components/supportAndScalability.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const SupportAndScalability = () => {
   return (
-    <div className="flex items-start gap-8 p-6 bg-white max-w-md">
+    <div
+      className="flex items-start gap-8 p-8 rounded-[24px] bg-white max-w-lg"
+      style={{ boxShadow: "0px 0px 15px 5px #17CDCA78 inset" }}
+    >
       <div className="flex-1">
         <div className="text-[30px] mb-2">24/7</div>
         <h3 className="text-[26px] font-bold  mb-2">Support</h3>
@@ -12,7 +15,7 @@ const SupportAndScalability = () => {
       </div>
       <div className="w-px h-24 bg-gray-300"></div>
       <div className="flex-1">
-        <div className="text-[30px] font-bold mb-2">100%</div>
+        <div className="text-[30px] mb-2">100%</div>
         <h3 className="text-[26px] font-bold mb-2">Scalability</h3>
         <p className="text-[22px]">
           Effortlessly grows with increasing demands and needs.
