6e77d9c85607083efa3de3fd69576edc19fc2bd4
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-13 18:27:32 +0530

    Add BlueGlow component to contact page for enhanced visual appeal

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,10 +1,11 @@
+import BlueGlow from "@/components/blueGlow";
 
 
 const Page = () => {
   return (
     <>
       <div className="flex flex-col items-center justify-center">
-        
+        <BlueGlow />
       </div>
     </>
   );
