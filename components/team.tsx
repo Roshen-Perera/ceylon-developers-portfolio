0dcdc5feacc461882517c77738a7a73eaa77adff
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-13 12:11:12 +0530

    Add Team component: implement initial layout and styling for team section

--- components/team.tsx
+++ components/team.tsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Team = () => {
   return (
-    <div>Team</div>
-  )
+    <div className="flex flex-col items-center justify-center mt-20">
+      <h1 className="text-7xl font-medium text-center">
+        Discover our <span className="font-kaushan"> Talented </span> and
+        <br />
+        <span className="font-kaushan"> Dedicated </span>team
+      </h1>
+    </div>
+  );
 }
 
 export default Team
