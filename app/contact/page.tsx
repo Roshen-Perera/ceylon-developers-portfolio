de53af3f13f4ad8d60600afe91110abce6ccfb7a
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-13 18:27:48 +0530

    Add NavBar component to contact page for improved navigation

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,4 +1,5 @@
 import BlueGlow from "@/components/blueGlow";
+import NavBar from "@/components/navBar";
 
 
 const Page = () => {
@@ -6,6 +7,7 @@ const Page = () => {
     <>
       <div className="flex flex-col items-center justify-center">
         <BlueGlow />
+        <NavBar />
       </div>
     </>
   );
