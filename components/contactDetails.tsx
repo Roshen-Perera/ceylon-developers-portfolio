ad1c236d87eaca639a5ec62690260f20db31dc28
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-14 17:52:26 +0530

    Refactor ContactDetails component layout for improved alignment and styling

--- components/contactDetails.tsx
+++ components/contactDetails.tsx
@@ -4,13 +4,13 @@ import React from 'react'
 const ContactDetails = () => {
   return (
     <div className="flex flex-row items-center justify-center mt-12">
-      <div className="flex flex-col items-start">
-        <div>
-            <Mail size={48} color="#00FFFF" />
+      <div className="flex flex-col items-center justify-center gap-4 mx-8">
+        <div className="p-5 bg-[#17CDCA]">
+          <Mail size={48} color="#00FFFF" />
         </div>
-        <h3 className="text-2xl font-semibold">Contact Information</h3>
-        <p className="text-lg">Email: example@gmail.com</p>
-        <p className="text-lg">Phone: (123) 456-7890</p>
+        <h3 className="text-[26px] font-medium">Email</h3>
+        <p className="text-[22px]">customer@gmail.com</p>
+        <p className="text-[22px]">client@automx.com</p>
       </div>
     </div>
   );
