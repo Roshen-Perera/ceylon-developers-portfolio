3dee5908e8adbbcddb4e4d7c6961d6ed1b52f22b
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-14 17:52:46 +0530

    Refactor ContactDetails component structure for improved readability

--- components/contactDetails.tsx
+++ components/contactDetails.tsx
@@ -5,12 +5,14 @@ const ContactDetails = () => {
   return (
     <div className="flex flex-row items-center justify-center mt-12">
       <div className="flex flex-col items-center justify-center gap-4 mx-8">
-        <div className="p-5 bg-[#17CDCA]">
-          <Mail size={48} color="#00FFFF" />
+        <div>
+          <div className="p-5 bg-[#17CDCA]">
+            <Mail size={48} color="#00FFFF" />
+          </div>
+          <h3 className="text-[26px] font-medium">Email</h3>
+          <p className="text-[22px]">customer@gmail.com</p>
+          <p className="text-[22px]">client@automx.com</p>
         </div>
-        <h3 className="text-[26px] font-medium">Email</h3>
-        <p className="text-[22px]">customer@gmail.com</p>
-        <p className="text-[22px]">client@automx.com</p>
       </div>
     </div>
   );
