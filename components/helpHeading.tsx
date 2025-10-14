7201e10023aa0e9826d294706c5e02e766eda73a
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-14 12:04:54 +0530

    Add HelpHeading component for user support section

---
+++ components/helpHeading.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const HelpHeading = () => {
+  return (
+    <div className="flex flex-col items-center justify-center mt-12">
+      <p className="text-7xl font-medium">
+        We are here to<span className="font-kaushan"> help</span>
+      </p>
+      <div className="text-[22px] mt-8 max-w-2xl flex items-center mx-auto px-4">
+        <p className="text-center">
+          Our team is ready to support you with expert advice & solutions.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default HelpHeading;
