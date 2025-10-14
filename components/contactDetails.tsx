873c3e41c5b27cc9a0d6bc3e428602c01a4199a1
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-14 18:04:30 +0530

    Refactor ContactDetails component to increase gap between sections for improved layout

--- components/contactDetails.tsx
+++ components/contactDetails.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const ContactDetails = () => {
   return (
     <div className="flex flex-row items-center justify-center mt-12">
-      <div className="flex flex-row gap-20">
+      <div className="flex flex-row gap-40">
         <div className="flex flex-col items-center gap-4">
           <div className="p-4 bg-[#17CDCA] rounded-4xl">
             <Mail size={20} color="#000000" />
