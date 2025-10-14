6a42f754057b440851fd14240c954c133e3f253e
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-14 17:56:18 +0530

    Refactor ContactDetails component for improved layout and styling

--- components/contactDetails.tsx
+++ components/contactDetails.tsx
@@ -5,13 +5,15 @@ const ContactDetails = () => {
   return (
     <div className="flex flex-row items-center justify-center mt-12">
       <div className="flex flex-col items-center justify-center gap-4 mx-8">
-        <div>
-          <div className="p-5 bg-[#17CDCA]">
-            <Mail size={48} color="#00FFFF" />
+        <div className="flex flex-col items-center gap-4">
+          <div className="p-4 bg-[#17CDCA] rounded-4xl">
+            <Mail size={20} color="#00FFFF" />
           </div>
-          <h3 className="text-[26px] font-medium">Email</h3>
-          <p className="text-[22px]">customer@gmail.com</p>
-          <p className="text-[22px]">client@automx.com</p>
+          <div className="space-y-2">
+            <h3 className="text-[26px] font-medium text-gray-800">Email</h3>
+            <p className="text-[22px] text-gray-600">customer@gmail.com</p>
+            <p className="text-[22px] text-gray-600">client@automx.com</p>
+          </div>
         </div>
       </div>
     </div>
