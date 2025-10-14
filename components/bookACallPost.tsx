2e9a68fdd68b9ea66bb650796cf4b6a4ca085b3e
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-14 18:07:29 +0530

    Add styling and structure to BookACallPost component

--- components/bookACallPost.tsx
+++ components/bookACallPost.tsx
@@ -2,10 +2,21 @@ import React from 'react'
 
 const BookACallPost = () => {
   return (
-    <div>
-      
+    <div className="flex flex-col gap-7 p-7 rounded-3xl items-center max-w-4xl text-center bg-gray-100">
+      <h1 className="font-medium text-7xl">
+        Let’s Turn Your
+        <br />
+        <span className="font-kaushan"> Dream </span> Into <span className="font-kaushan"> Reality </span>
+      </h1>
+      <p className="text-[22px]">
+        Pushing the boundaries of what&apos;s possible in the digital realm.
+        Join us on this journey into the future of technology.
+      </p>
+      <button className="text-[22px] w-[172px] p-3 text-lg bg-[#17CDCA] rounded-2xl">
+        Get Started
+      </button>
     </div>
-  )
+  );
 }
 
 export default BookACallPost
