bb2c3bb9c4820ab8221cc2b5c9663b971dfb621b
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-14 17:56:53 +0530

    Refactor ContactDetails component for improved text alignment

--- components/contactDetails.tsx
+++ components/contactDetails.tsx
@@ -9,7 +9,7 @@ const ContactDetails = () => {
           <div className="p-4 bg-[#17CDCA] rounded-4xl">
             <Mail size={20} color="#00FFFF" />
           </div>
-          <div className="space-y-2">
+          <div className="text-center">
             <h3 className="text-[26px] font-medium text-gray-800">Email</h3>
             <p className="text-[22px] text-gray-600">customer@gmail.com</p>
             <p className="text-[22px] text-gray-600">client@automx.com</p>
