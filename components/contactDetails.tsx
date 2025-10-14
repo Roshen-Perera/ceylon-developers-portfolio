8e3ed9fdb0d6f019d20bd611a884380ffe5b89c2
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-14 17:48:26 +0530

    Refactor ContactDetails component layout for improved structure and styling

--- components/contactDetails.tsx
+++ components/contactDetails.tsx
@@ -1,11 +1,20 @@
+import { Mail } from 'lucide-react';
+import Image from 'next/image';
 import React from 'react'
 
 const ContactDetails = () => {
   return (
-    <div>
-      
+    <div className="flex flex-row items-center justify-center mt-12">
+      <div className="flex flex-col items-start">
+        <div>
+            <Mail size={48} color="#00FFFF" />
+        </div>
+        <h3 className="text-2xl font-semibold">Contact Information</h3>
+        <p className="text-lg">Email: example@gmail.com</p>
+        <p className="text-lg">Phone: (123) 456-7890</p>
+      </div>
     </div>
-  )
+  );
 }
 
 export default ContactDetails
