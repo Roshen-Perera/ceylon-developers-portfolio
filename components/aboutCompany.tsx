9f466655aa763c7deb5822d103bb0854ed72782a
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-15 13:47:57 +0530

    Fix formatting in AboutCompany component and ensure content is properly structured

--- components/aboutCompany.tsx
+++ components/aboutCompany.tsx
@@ -4,11 +4,18 @@ const AboutCompany = () => {
   return (
     <div className="flex flex-col items-center justify-center mt-20">
       <div className="grid grid-cols-2">
-        <h1 className='text-7xl font-medium'>
+        <h1 className="text-7xl font-medium">
           Discover Why Ceylon Developer is the Premier Destination for Advancing
           Your Career
         </h1>
-        <div></div>
+        <div>
+          <div>
+            <p>We are growing fast.</p>
+            <p>Flexible hours.</p>
+            <p>Work from anywhere.</p>
+            <p>Paid vacations.</p>
+          </div>
+        </div>
       </div>
     </div>
   );
