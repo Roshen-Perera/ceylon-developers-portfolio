56e86f6a5b6eb267fa220cc82ade9520f9662526
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-15 14:18:29 +0530

    Refactor AboutCompany component for improved layout and button styling

--- components/aboutCompany.tsx
+++ components/aboutCompany.tsx
@@ -8,7 +8,7 @@ const AboutCompany = () => {
           Discover Why Ceylon Developer is the Premier Destination for Advancing
           Your Career
         </h1>
-        <div>
+        <div className="flex flex-col items-start">
           <div>
             <p>We are growing fast.</p>
             <p>Flexible hours.</p>
@@ -20,8 +20,8 @@ const AboutCompany = () => {
             eiusmod tempor incididunt ut labore et dolore magna aliqua. Viverra
             justo nec ultrices dui sapien eget mi proin sed.{" "}
           </p>
-          <button>
-            
+          <button className="text-[22px] w-[172px] p-3 text-lg bg-[#17CDCA] rounded-2xl">
+            About Company
           </button>
         </div>
       </div>
