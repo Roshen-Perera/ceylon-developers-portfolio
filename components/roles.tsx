cb7133beb8ca07980152ab49f1653f1d36ebc601
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-15 17:23:08 +0530

    Refactor Roles component layout and styling for improved readability

--- components/roles.tsx
+++ components/roles.tsx
@@ -7,21 +7,23 @@ const Roles = () => {
         Find the <span className="font-kaushan"> Best Role </span> for You
       </h1>
       <div className="grid grid-cols-2 ">
-        <div>
+        <div className="flex flex-col border-1 p-8 m-8 rounded-3xl gap-6 max-w-2xl">
           <h3 className="text-[30px] font-medium">Software Developer</h3>
-          <p>
+          <p className="text-[22px]">
             We seek a skilled Software Developer to join our tech team. You will
             be responsible for developing and maintaining software applications.
             Strong programming skills and experience with various coding
             languages are required.
           </p>
           <div className="flex flex-row justify-between">
-            <div>
-                <p>Remote</p>
-                <p>Full-Time</p>
+            <div className="text-[22px] gap-4">
+              <p>Remote</p>
+              <p>Full-Time</p>
             </div>
             <div>
-                
+              <button className="text-[22px] p-3 text-lg bg-[#17CDCA] rounded-3xl">
+                Apply Now
+              </button>
             </div>
           </div>
         </div>
