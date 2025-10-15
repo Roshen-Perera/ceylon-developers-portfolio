1654aa3e22e56d7f99600877d48e5f78d6b8f9e8
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-15 17:13:07 +0530

    Refactor Roles component structure and formatting

--- components/roles.tsx
+++ components/roles.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Roles = () => {
   return (
-    <div>
-      
+    <div className="flex flex-col items-center justify-center mt-20">
+      <h1 className="text-7xl font-medium">
+        Find the <span className="font-kaushan"> Best Role </span> for You
+      </h1>
     </div>
-  )
-}
+  );
+};
 
-export default Roles
+export default Roles;
