79d9ec85f51d38b14a50f06aada89da374268958
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-15 14:48:43 +0530

    Add steps to HiringProcess component for detailed hiring stages

--- components/hiringProcess.tsx
+++ components/hiringProcess.tsx
@@ -1,6 +1,38 @@
+import { Award, ClipboardList, FileText, Users } from 'lucide-react';
 import React from 'react'
 
 const HiringProcess = () => {
+  const steps = [
+    {
+      icon: FileText,
+      title: "Apply with Your CV",
+      description:
+        "We begin by understanding your business.Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      position: "right",
+    },
+    {
+      icon: ClipboardList,
+      title: "Initial Screening",
+      description:
+        "We begin by understanding your business.Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      position: "left",
+    },
+    {
+      icon: Award,
+      title: "Skills Assessment",
+      description:
+        "We begin by understanding your business.Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      position: "right",
+    },
+    {
+      icon: Users,
+      title: "Final Interview",
+      description:
+        "We begin by understanding your business.Lorem ipsum dolor sit amet, consectetur adipiscing elit",
+      position: "left",
+    },
+  ];
+
   return (
     <>
       <div className="flex flex-col items-center justify-center mt-20">
