8b7ca8fe5ea74c8cbdca95f1ecb12a22f5a8b900
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-15 17:16:55 +0530

    Add job description for Software Developer role in Roles component

--- components/roles.tsx
+++ components/roles.tsx
@@ -6,6 +6,26 @@ const Roles = () => {
       <h1 className="text-7xl font-medium">
         Find the <span className="font-kaushan"> Best Role </span> for You
       </h1>
+      <div className="grid grid-cols-2 ">
+        <div>
+          <h3 className="text-[30px] font-medium">Software Developer</h3>
+          <p>
+            We seek a skilled Software Developer to join our tech team. You will
+            be responsible for developing and maintaining software applications.
+            Strong programming skills and experience with various coding
+            languages are required.
+          </p>
+          <div className="flex flex-row justify-between">
+            <div>
+                <p>Remote</p>
+                <p>Full-Time</p>
+            </div>
+            <div>
+                
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
