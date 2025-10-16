fa64d784ad205850016723b1413b583a3cd14a28
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-16 15:13:22 +0530

    Refactor Studies component for improved styling and layout consistency

--- components/studies.tsx
+++ components/studies.tsx
@@ -3,14 +3,18 @@ import React from 'react'
 const Studies = () => {
   return (
     <div className="flex flex-col items-center justify-center mt-20">
-        <div className='grid grid-cols-2 gap-8 px-4'>
-            <div className='flex flex-row justify-between'>
-                <div>Framer Development</div>
-                <div>Figma Design</div>
-            </div>
+      <div className="grid grid-cols-2 gap-8 px-4">
+        <div className="flex flex-row justify-between">
+          <div className="p-2 rounded-[1000px] border-1 border-[#17CDCA75]">
+            Framer Development
+          </div>
+          <div className="p-2 rounded-[1000px] border-1 border-[#17CDCA75]">
+            Figma Design
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Studies
