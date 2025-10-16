eaf2ed0d022893dee730ffd7aa32768e79b49bc0
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-16 15:12:09 +0530

    Refactor Studies component layout for improved structure and styling

--- components/studies.tsx
+++ components/studies.tsx
@@ -3,7 +3,12 @@ import React from 'react'
 const Studies = () => {
   return (
     <div className="flex flex-col items-center justify-center mt-20">
-        <div className='flex flex-row'></div>
+        <div className='grid grid-cols-2 gap-8 px-4'>
+            <div className='flex flex-row justify-between'>
+                <div>Framer Development</div>
+                <div>Figma Design</div>
+            </div>
+        </div>
     </div>
   )
 }
