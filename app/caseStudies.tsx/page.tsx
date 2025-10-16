982b23c78b4b0a968de92ef2e17d7514476bfa2d
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-16 14:32:48 +0530

    Refactor Page component to include BlueGlow and improve layout structure

--- app/caseStudies.tsx/page.tsx
+++ app/caseStudies.tsx/page.tsx
@@ -1,9 +1,12 @@
+import BlueGlow from '@/components/blueGlow';
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>Page</div>
-  )
+    <div className="flex flex-col items-center justify-center">
+      <BlueGlow />
+    </div>
+  );
 }
 
-export default Page 
+export default Page;
