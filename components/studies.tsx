8ba3c52c3297ed55779a446a210495acbc29a2e7
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-16 15:18:11 +0530

    Refactor Studies component layout for improved spacing between items

--- components/studies.tsx
+++ components/studies.tsx
@@ -4,7 +4,7 @@ const Studies = () => {
   return (
     <div className="flex flex-col items-center justify-center mt-20">
       <div className="grid grid-cols-2 gap-8 px-4">
-        <div className="flex flex-row justify-between">
+        <div className="flex flex-row justify-between gap-5">
           <div className="p-2 rounded-[1000px] border-1 border-[#17CDCA75]">
             Framer Development
           </div>
