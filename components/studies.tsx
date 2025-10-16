35dff0e7f5ae4a57b49a165ccc4250cbb1882019
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-16 15:20:17 +0530

    Refactor Studies component layout to improve structure and readability

--- components/studies.tsx
+++ components/studies.tsx
@@ -4,13 +4,23 @@ const Studies = () => {
   return (
     <div className="flex flex-col items-center justify-center mt-20">
       <div className="grid grid-cols-2 gap-8 px-4">
-        <div className="flex flex-row justify-between gap-5">
-          <div className="p-2 rounded-[1000px] border-1 border-[#17CDCA75]">
-            Framer Development
+        <div className="flex flex-col">
+          <div className="flex flex-row justify-between gap-5">
+            <div className="p-2 rounded-[1000px] border-1 border-[#17CDCA75]">
+              Framer Development
+            </div>
+            <div className="p-2 rounded-[1000px] border-1 border-[#17CDCA75]">
+              Figma Design
+            </div>
           </div>
-          <div className="p-2 rounded-[1000px] border-1 border-[#17CDCA75]">
-            Figma Design
-          </div>
+          <h2 className="text-[50px] font-bold">Saas Website</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Viverra
+            justo nec ultrices dui sapien eget mi proin sed. Eu lobortis
+            elementum nibh tellus molestie nunc non blandit massa.
+          </p>
+          
         </div>
       </div>
     </div>
