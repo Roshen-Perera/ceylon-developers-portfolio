4ce439f951ae86c9d887fddbc1fe06fec12beb64
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-17 16:49:52 +0530

    Added heading

--- components/projectMembers.tsx
+++ components/projectMembers.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const ProjectMembers = () => {
   return (
-    <div>
-      
+    <div className="flex flex-col gap-8">
+      <h1 className="text-7xl font-medium">Team Members Involved</h1>
     </div>
-  )
+  );
 }
 
 export default ProjectMembers
