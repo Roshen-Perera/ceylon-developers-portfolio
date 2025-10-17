899ba7cd612d8211ff27043e357e9919ea4dcadd
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-17 17:18:29 +0530

    Added padding top for saas2 pic

--- components/relatedSaasWorks.tsx
+++ components/relatedSaasWorks.tsx
@@ -5,9 +5,15 @@ const RelatedSaasWorks = () => {
   return (
     <div className="flex flex-col gap-8">
       <h1 className="text-7xl font-medium">Related Works</h1>
-      <div className='flex flex-row justify-around'>
-        <Image src='/assets/images/saas1.png' alt='' width={483} height={539}/>
-        <Image src='/assets/images/saas2.png' alt='' width={483} height={539}/>
+      <div className="flex flex-row justify-around">
+        <Image src="/assets/images/saas1.png" alt="" width={483} height={539} />
+        <Image
+          src="/assets/images/saas2.png"
+          alt=""
+          width={483}
+          height={539}
+          className="pt-30"
+        />
       </div>
     </div>
   );
