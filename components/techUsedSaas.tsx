b51eb5fc938e58d751769547bf5a3c92035dee3e
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-17 16:38:27 +0530

    Add paragraph

--- components/techUsedSaas.tsx
+++ components/techUsedSaas.tsx
@@ -4,6 +4,15 @@ const TechUsedSaas = () => {
   return (
     <div className="flex flex-col gap-8">
       <h1 className="text-7xl font-medium">Technologies Used</h1>
+      <p className="text-[22px] max-w-[1200px]">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Viverra justo nec
+        ultrices dui sapien eget mi proin sed. Eu lobortis elementum nibh tellus
+        molestie nunc non blandit massa. Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Viverra justo nec ultrices dui sapien eget mi proin sed.
+        Eu lobortis elementum nibh tellus molestie nunc non blandit massa.
+      </p>
     </div>
   );
 };
