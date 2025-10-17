681d39f024fc7e65ba68cde0991efeda5a9bf737
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-17 13:18:06 +0530

    Add project details section to projects page

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -7,6 +7,17 @@ const Page = () => {
     <div className="flex flex-col items-center justify-center">
       <BlueGlow />
       <NavBar />
+      <div className="mt-15">
+        <div className="flex flex-col mt-8 z-20 items-center">
+          <p className="font-semibold text-8xl ">Project Details</p>
+        </div>
+        <div className="text-lg mt-8 max-w-2xl flex items-center mx-auto px-4">
+          <p className="text-[22px] text-center">
+            Pushing the boundaries of what&apos;s possible in the digital realm.
+            Join us on this journey into the future of technology.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
