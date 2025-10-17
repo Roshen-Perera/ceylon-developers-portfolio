29b16ff9d5f397e508022c1dc90e92b71d9e10ff
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-17 16:29:59 +0530

    Added heading Technologies Used

--- components/techUsedSaas.tsx
+++ components/techUsedSaas.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const TechUsedSaas = () => {
   return (
     <div className="flex flex-col gap-8">
-      <h1 className="text-7xl font-medium">SaaS Website</h1>
+      <h1 className="text-7xl font-medium">Technologies Used</h1>
     </div>
   );
 };
