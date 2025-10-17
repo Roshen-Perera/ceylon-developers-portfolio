7b35af33ed2ba3518487b9c8373746678c364db9
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-17 10:43:59 +0530

    Refactor Page component layout to center content and include BlueGlow component

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,11 +1,12 @@
+import BlueGlow from '@/components/blueGlow';
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>
-      
+    <div className="flex flex-col items-center justify-center">
+      <BlueGlow />
     </div>
-  )
+  );
 }
 
 export default Page
