0e5268540902c783a77faa99e43594dbf0111173
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-17 15:57:50 +0530

    Added SaaS Website heading

---
+++ components/saasWebsite.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SaasWebsite = () => {
+  return (
+    <div>
+      <h1 className="text-7xl font-medium">SaaS Website</h1>
+    </div>
+  );
+}
+
+export default SaasWebsite
