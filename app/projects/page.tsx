524807fd1d4f66d2bd85bcc24071b3c024db866f
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-17 13:14:35 +0530

    Add NavBar component to projects page layout

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,10 +1,12 @@
 import BlueGlow from '@/components/blueGlow';
+import NavBar from '@/components/navBar';
 import React from 'react'
 
 const Page = () => {
   return (
     <div className="flex flex-col items-center justify-center">
       <BlueGlow />
+      <NavBar />
     </div>
   );
 }
