833916ce352474bc6e71301b142191a0f7eec760
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-17 16:42:17 +0530

    created technav component and added links

---
+++ components/techNav.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const TechNav = () => {
+  return (
+    <div>
+      <div className="grid grid-cols-5">
+        <h3 className="text-[30px]">Fronted</h3>
+        <h3 className="text-[30px]">Backend</h3>
+        <h3 className="text-[30px]">Vcs</h3>
+        <h3 className="text-[30px]">Hosting/Deployement</h3>
+      </div>
+    </div>
+  );
+}
+
+export default TechNav
