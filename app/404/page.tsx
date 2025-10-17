e2aec03b1c1cbcf3c864476e7e816e8fd822e218
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-17 17:26:39 +0530

    Addec 404 image

--- app/404/page.tsx
+++ app/404/page.tsx
@@ -1,5 +1,6 @@
 import BlueGlow from "@/components/blueGlow";
 import NavBar from "@/components/navBar";
+import Image from "next/image";
 import React from "react";
 
 const Page = () => {
@@ -7,6 +8,13 @@ const Page = () => {
     <div className="flex flex-col items-center justify-center">
       <BlueGlow />
       <NavBar />
+      <Image
+        src="/assets/images/404.png"
+        alt="404"
+        width={696.3260498046875}
+        height={266.9766845703125}
+        className="mt-20"
+      />
     </div>
   );
 };
