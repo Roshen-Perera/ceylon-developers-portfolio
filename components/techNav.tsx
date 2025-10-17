c3efc5af3a6ac557de3fe6734ba87a84b6ed1ebc
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-17 16:44:00 +0530

    Added react and javascript cards

--- components/techNav.tsx
+++ components/techNav.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 const TechNav = () => {
   return (
@@ -9,8 +10,26 @@ const TechNav = () => {
         <h3 className="text-[30px]">Vcs</h3>
         <h3 className="text-[30px]">Hosting/Deployement</h3>
       </div>
+      <div className="flex flex-row mt-10">
+        <div className="w-[223px] h-[92px] border border-gray-300 rounded-2xl flex items-center justify-center">
+          <Image
+            src={"/assets/icons/technologies/javascript.png"}
+            alt="javascript"
+            width={50}
+            height={50}
+          />
+        </div>
+        <div className="w-[223px] h-[92px] border border-gray-300 rounded-2xl flex items-center justify-center">
+          <Image
+            src={"/assets/icons/technologies/react.png"}
+            alt="react"
+            width={50}
+            height={50}
+          />
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default TechNav
+export default TechNav;
