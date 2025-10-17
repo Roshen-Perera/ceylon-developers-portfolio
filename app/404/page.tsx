cf7996ba5180f3ae06782cb22deec0c9325bc4b6
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-17 17:28:16 +0530

    Added paragraph and button

--- app/404/page.tsx
+++ app/404/page.tsx
@@ -15,6 +15,18 @@ const Page = () => {
         height={266.9766845703125}
         className="mt-20"
       />
+      <div className="text-lg mt-10 max-w-2xl flex items-center mx-auto px-4">
+        <p className="text-[22px] text-center">
+          We’re sorry. the page you requested could no be found Please go back
+          to the home page
+        </p>
+      </div>
+      <div className="mt-8 flex max-w-4xl gap-10 justify-center items-center">
+        <button className="text-[22px] p-3 text-lg bg-[#17CDCA] rounded-2xl">
+          Back to Home
+        </button>
+      </div>
+      
     </div>
   );
 };
