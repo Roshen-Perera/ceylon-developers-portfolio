17e6cdf2036890c59f3ff84193e1b58af4facfb2
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-17 16:00:10 +0530

    Added paragraph

--- components/saasWebsite.tsx
+++ components/saasWebsite.tsx
@@ -4,6 +4,15 @@ const SaasWebsite = () => {
   return (
     <div>
       <h1 className="text-7xl font-medium">SaaS Website</h1>
+      <p className="text-[22px] max-w-[1200px]">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Viverra justo nec
+        ultrices dui sapien eget mi proin sed. Eu lobortis elementum nibh tellus
+        molestie nunc non blandit massa. Lorem ipsum dolor sit amet, consectetur
+        adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
+        magna aliqua. Viverra justo nec ultrices dui sapien eget mi proin sed.
+        Eu lobortis elementum nibh tellus molestie nunc non blandit massa.
+      </p>
     </div>
   );
 }
