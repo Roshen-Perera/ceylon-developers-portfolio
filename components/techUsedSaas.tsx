ce13a03734b07fbab699cd014d94f66616687e41
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-17 16:28:59 +0530

    created techUsedSaas component

---
+++ components/techUsedSaas.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TechUsedSaas = () => {
+  return (
+    <div className="flex flex-col gap-8">
+      <h1 className="text-7xl font-medium">SaaS Website</h1>
+    </div>
+  );
+};
+
+export default TechUsedSaas;
