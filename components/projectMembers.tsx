d3e697fd3587bc107b850ebce24ced3b9b68561a
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-17 16:55:58 +0530

    Added a member

--- components/projectMembers.tsx
+++ components/projectMembers.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 const ProjectMembers = () => {
   return (
     <div className="flex flex-col gap-8">
       <h1 className="text-7xl font-medium">Team Members Involved</h1>
+      <div className="items-center">
+        <Image
+          src={"/assets/images/michealr.png"}
+          alt="michealr"
+          width={322}
+          height={352}
+        />
+        <h5 className="text-[30px] font-bold">Michael R</h5>
+        <p className="22px text-[22px] text-[#5C5C5C]">Software Developer</p>
+      </div>
     </div>
   );
-}
+};
 
-export default ProjectMembers
+export default ProjectMembers;
