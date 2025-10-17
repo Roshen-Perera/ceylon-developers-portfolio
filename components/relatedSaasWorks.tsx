8f96c7b3fd8adb1735344323717666d79c36e136
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-17 17:17:01 +0530

    Added two images to RelatedSaasWorks compnent

--- components/relatedSaasWorks.tsx
+++ components/relatedSaasWorks.tsx
@@ -1,11 +1,16 @@
+import Image from 'next/image';
 import React from 'react'
 
 const RelatedSaasWorks = () => {
   return (
-    <div>
-      
+    <div className="flex flex-col gap-8">
+      <h1 className="text-7xl font-medium">Related Works</h1>
+      <div className='flex flex-row justify-around'>
+        <Image src='/assets/images/saas1.png' alt='' width={483} height={539}/>
+        <Image src='/assets/images/saas2.png' alt='' width={483} height={539}/>
+      </div>
     </div>
-  )
+  );
 }
 
 export default RelatedSaasWorks
