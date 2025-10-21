c88c06ddbf624131d91e3f029c6e9fe373130e82
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-21 11:08:14 +0530

    Add Privacy Policy section to privacy page

--- app/privacy/page.tsx
+++ app/privacy/page.tsx
@@ -6,9 +6,21 @@ const Page = () => {
   return (
     <div className="flex flex-col items-center justify-center">
       <BlueGlow />
-      <NavBar/>
+      <NavBar />
+      <div className="mt-15">
+        <div className="flex flex-col mt-8 z-20 items-center">
+          <p className="font-semibold text-8xl ">Privacy Policy</p>
+        </div>
+        <div className="text-lg mt-8 max-w-3xl flex items-center mx-auto px-4">
+          <p className="text-[22px] text-center">
+            Pushing the boundaries of what&apos;s possible in the digital realm.
+            <br />
+            Join us on this journey into the future of technology.
+          </p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Page 
