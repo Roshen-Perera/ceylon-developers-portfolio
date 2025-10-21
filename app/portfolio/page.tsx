b3ffa0b9f8f85d8e82fa5736e15e46246607290b
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-21 18:08:10 +0530

    Added navBar component

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,8 +1,10 @@
+import NavBar from '@/components/navBar'
 import React from 'react'
 
 const Page = () => {
   return (
     <div className="flex flex-col items-center justify-center">
+        <NavBar/>
     </div>
   )
 }
