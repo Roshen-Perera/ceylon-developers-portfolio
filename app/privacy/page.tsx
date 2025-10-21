ccc1a3a3d1cc403882697cb46b4b00b8b0686797
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-21 10:46:30 +0530

    Add privacy page component with BlueGlow integration

---
+++ app/privacy/page.tsx
@@ -0,0 +1,12 @@
+import BlueGlow from '@/components/blueGlow'
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <BlueGlow />
+    </div>
+  )
+}
+
+export default Page 
