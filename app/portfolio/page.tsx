99120d28af92c64e59a787664a74a55d9a1df287
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-22 14:39:46 +0530

    Add rating stars and experience badge to portfolio page

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -54,7 +54,32 @@ const Page = () => {
             width={487}
             height={636}
           />
-          <div></div>
+          <div>
+            <div className="relative z-10 text-right w-full pr-8">
+              {/* Rating Stars */}
+              <div className="flex gap-1 justify-end mb-2">
+                {[...Array(5)].map((_, i) => (
+                  <svg
+                    key={i}
+                    className="w-5 h-5 fill-yellow-400"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z" />
+                  </svg>
+                ))}
+              </div>
+
+              {/* Experience Badge */}
+              <div className="text-right">
+                <div className="text-[50px] font-bold text-gray-900">
+                  5 Years
+                </div>
+                <div className="text-base text-gray-600 font-medium">
+                  Experience
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
