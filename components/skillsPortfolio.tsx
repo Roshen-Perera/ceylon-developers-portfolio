57f9753cfb56b3158930b2abfb968f796887b15a
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-22 18:15:01 +0530

    Refactor SkillsPortfolio component to dynamically render skills from the skills array

--- components/skillsPortfolio.tsx
+++ components/skillsPortfolio.tsx
@@ -31,15 +31,17 @@ const SkillsPortfolio = () => {
         Here are some of my skills on which I have been working.{" "}
       </p>
       <div className="flex flex-wrap mt-10 max-w-[1109px] gap-5 items-center justify-center">
-        <div className="flex flex-row gap-2 rounded-[45px] py-3 px-8 border-1">
-          <Image
-            src="/assets/icons/technologies/html5.png"
-            width={35}
-            height={40}
-            alt="js"
-          />
-          <p className="text-[30px]">HTML5</p>
-        </div>
+        {skills.map((skill, index) => (
+          <div key={index} className="flex flex-row gap-2 rounded-[45px] py-3 px-8 border-1">
+            <Image
+              src={skill.icon}
+              width={35}
+              height={40}
+              alt={skill.name}
+            />
+            <p className="text-[30px]">{skill.name}</p>
+          </div>
+        ))}
       </div>
     </div>
   );
