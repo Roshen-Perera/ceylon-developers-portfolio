587561b16c2b7453bad85d6c8bf032878f7e99d3
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-22 18:01:41 +0530

    Add new technology icons f\

---
+++ components/skillsPortfolio.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const SkillsPortfolio = () => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-7xl font-bold mt-20">Skills</h1>
+      <p className="text-[22px] mt-10">
+        Here are some of my skills on which I have been working.{" "}
+      </p>
+      <div className='flex flex-wrap'>
+            
+      </div>
+    </div>
+  );
+}
+
+export default SkillsPortfolio
