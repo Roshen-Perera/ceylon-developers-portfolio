c405d4d48a85850c634e5bad21add119b207b479
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-22 13:23:25 +0530

    Added greeting message to portfolio page

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -4,12 +4,14 @@ import React from 'react'
 
 const Page = () => {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <NavBarPortfolio />
-      <div className="text-lg mt-8 max-w-3xl flex items-center mx-auto px-4">
-        
+    <>
+      <div className="flex flex-col items-center justify-center">
+        <NavBarPortfolio />
       </div>
-    </div>
+      <div className="mt-20 max-w-[1187px]">
+        <p className="text-[22px]">️👋 Hello I am,</p>
+      </div>
+    </>
   );
 }
 
