339f6f522c12ce1edf65e1a45d77f567ac5922ea
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-22 13:05:29 +0530

    Added container div for layout in portfolio page

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -6,6 +6,9 @@ const Page = () => {
   return (
     <div className="flex flex-col items-center justify-center">
       <NavBarPortfolio />
+      <div className="text-lg mt-8 max-w-3xl flex items-center mx-auto px-4">
+        
+      </div>
     </div>
   );
 }
