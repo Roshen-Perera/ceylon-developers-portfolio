836c7a13499f54492a1c438921f37261788c4a05
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-22 18:04:02 +0530

    Refactor SkillsPortfolio component layout for better structure and styling

--- components/skillsPortfolio.tsx
+++ components/skillsPortfolio.tsx
@@ -8,15 +8,15 @@ const SkillsPortfolio = () => {
       <p className="text-[22px] mt-10">
         Here are some of my skills on which I have been working.{" "}
       </p>
-      <div className="flex flex-wrap rounded-3xl py-5 px-5">
-        <div className="flex flex-row gap-2">
+      <div className="flex flex-wrap">
+        <div className="flex flex-row gap-2 rounded-3xl py-5 px-5">
           <Image
             src="/assets/icons/html5.png"
             width={35}
             height={40}
             alt="js"
           />
-          <p className='text-[30px]'>HTML5</p>
+          <p className="text-[30px]">HTML5</p>
         </div>
       </div>
     </div>
