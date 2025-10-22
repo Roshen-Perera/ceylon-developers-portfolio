b3d16c3ef46edffd02298c82333f376d336a442d
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-22 13:50:13 +0530

    Refactor layout structure and formatting in portfolio page

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,6 +1,5 @@
-
-import NavBarPortfolio from '@/components/navBarPortfolio';
-import React from 'react'
+import NavBarPortfolio from "@/components/navBarPortfolio";
+import React from "react";
 
 const Page = () => {
   return (
@@ -8,11 +7,18 @@ const Page = () => {
       <div className="flex flex-col items-center justify-center">
         <NavBarPortfolio />
       </div>
-      <div className="mt-20 max-w-[1187px]">
-        <p className="text-[22px]">️👋 Hello I am,</p>
+      <div className="flex flex-col mt-20 ml-30 max-w-[1103px] justify-center">
+        <div className="flex flex-col">
+          <p className="text-[22px]">️👋 Hello I am,</p>
+          <h2 className="text-[50px]">UI/UX designer </h2>
+          <p className="text-[22px]">
+            For the past decade, I’ve been on a mission to revolutionize design
+            and tech with bold creativity and cutting-edge solutions.{" "}
+          </p>
+        </div>
       </div>
     </>
   );
-}
+};
 
-export default Page
+export default Page;
