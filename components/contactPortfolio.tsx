75f257e6c30ab0c15f4512606d9798819a6ecb37
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-23 12:36:44 +0530

    Update heading in ContactPortfolio component to reflect correct section title

--- components/contactPortfolio.tsx
+++ components/contactPortfolio.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const ContactPortfolio = () => {
   return (
     <div className="flex flex-col items-center justify-center">
-      <h1 className="text-7xl font-bold mt-20">Skills</h1>
+      <h1 className="text-7xl font-bold mt-20">Contact Me</h1>
       <p className="text-[22px] mt-10">
         Cultivating Connections: Reach Out and Connect with Me
       </p>
