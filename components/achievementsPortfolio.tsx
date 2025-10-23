64ce31051929798cd42b8581c78c71f8d4136bef
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-23 10:58:56 +0530

    Update gap spacing in AchievementsPortfolio component

--- components/achievementsPortfolio.tsx
+++ components/achievementsPortfolio.tsx
@@ -5,7 +5,7 @@ const AchievementsPortfolio = () => {
     <div className="flex flex-col items-center justify-center">
       <h1 className="text-7xl font-bold mt-20">Skills</h1>
       <div>
-        <div className="flex flex-row gap-2 justify-center items-center">
+        <div className="flex flex-row gap-3 justify-center items-center">
           <div className="w-[55px] h-[55px] rounded-4xl bg-gray-200 flex items-center justify-center p-4 mt-10 mb-10">
             <svg
               width="22"
