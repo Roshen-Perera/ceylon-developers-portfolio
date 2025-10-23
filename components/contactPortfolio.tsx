99b8e841d9b3f0457da652996f65980418b84f83
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-23 12:36:26 +0530

    Refactor ContactPortfolio component to display skills and connection message

--- components/contactPortfolio.tsx
+++ components/contactPortfolio.tsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
 
 const ContactPortfolio = () => {
   return (
-    <div>ContactPortfolio</div>
-  )
-}
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-7xl font-bold mt-20">Skills</h1>
+      <p className="text-[22px] mt-10">
+        Cultivating Connections: Reach Out and Connect with Me
+      </p>
+    </div>
+  );
+};
 
-export default ContactPortfolio
+export default ContactPortfolio;
