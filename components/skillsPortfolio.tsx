bd8be1bff22d5453eab4702419f3a77ea8b22b53
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-23 10:12:14 +0530

    Change the width and height in icons

--- components/skillsPortfolio.tsx
+++ components/skillsPortfolio.tsx
@@ -3,25 +3,71 @@ import React from 'react'
 
 const SkillsPortfolio = () => {
     const skills = [
-      { name: "HTML5", icon: "/assets/icons/technologies/html5.png" },
-      { name: "CSS3", icon: "/assets/icons/technologies/css3.png" },
-      { name: "JavaScript", icon: "/assets/icons/technologies/js.svg" },
-      { name: "React JS", icon: "/assets/icons/technologies/reactjs.png" },
-      { name: "Figma", icon: "/assets/icons/technologies/figma.svg" },
+      {
+        name: "HTML5",
+        icon: "/assets/icons/technologies/html5.png",
+        width: 35,
+        height: 40,
+      },
+      {
+        name: "CSS3",
+        icon: "/assets/icons/technologies/css3.png",
+        width: 35,
+        height: 40,
+      },
+      {
+        name: "JavaScript",
+        icon: "/assets/icons/technologies/js.svg",
+        width: 41.125,
+        height: 41.125,
+      },
+      {
+        name: "React JS",
+        icon: "/assets/icons/technologies/reactjs.png",
+        width: 42.00004577636719,
+        height: 44.39130783081055,
+      },
+      {
+        name: "Figma",
+        icon: "/assets/icons/technologies/figma.svg",
+        width: 41,
+        height: 40,
+      },
       {
         name: "Illustrator",
         icon: "/assets/icons/technologies/ai.png",
+        width: 44.153846740722656,
+        height: 42.077579498291016,
       },
-      { name: "Fire Base", icon: "/assets/icons/technologies/firebase.png" },
-      { name: "Adobe XD", icon: "/assets/icons/technologies/xd.png" },
-      { name: "Postman", icon: "/assets/icons/technologies/postman.svg" },
+      {
+        name: "Fire Base",
+        icon: "/assets/icons/technologies/firebase.png",
+        width: 30,
+        height: 40,
+      },
+      {
+        name: "Adobe XD",
+        icon: "/assets/icons/technologies/xd.png",
+        width: 42,
+        height: 40.02501678466797,
+      },
+      {
+        name: "Postman",
+        icon: "/assets/icons/technologies/postman.svg",
+        width: 40,
+        height: 40,
+      },
       {
         name: "VS Code",
         icon: "/assets/icons/technologies/vscode.png",
+        width: 40,
+        height: 40,
       },
       {
         name: "Photoshop",
         icon: "/assets/icons/technologies/ps.png",
+        width: 39,
+        height: 40,
       },
     ];
   return (
