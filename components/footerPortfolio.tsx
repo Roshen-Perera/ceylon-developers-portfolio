86f8d91eea6818ab31b3347747db6bab1c14501d
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-23 17:28:56 +0530

    Refactor FooterPortfolio component for improved layout and spacing

--- components/footerPortfolio.tsx
+++ components/footerPortfolio.tsx
@@ -5,12 +5,12 @@ const FooterPortfolio = () => {
   return (
     <>
       <div className="flex flex-col items-center justify-center">
-        <div className="flex flex-row gap-5 text-[22px] text-[#585858]">
-          <div className="flex flex-row gap-2">
+        <div className="flex flex-row gap-10 text-[22px] text-[#585858]">
+          <div className="flex flex-row gap-2 justify-center items-center">
             <Mail />
             <p>example@gmail.com</p>
           </div>
-          <div className="flex flex-row gap-2">
+          <div className="flex flex-row gap-2 justify-center items-center">
             <Phone />
             <p>+94 715 372 410</p>
           </div>
