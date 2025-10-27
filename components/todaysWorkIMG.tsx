9e633012cd6c2cc32301e63367e19e7fd6c34b5b
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-27 21:24:35 +0530

    Refactor TodaysWorkIMG component to display dynamic images based on theme

--- components/todaysWorkIMG.tsx
+++ components/todaysWorkIMG.tsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
 
 const TodaysWorkIMG = () => {
   return (
-    <div>TodaysWorkIMG</div>
-  )
-}
+    <div>
+      <Image
+        src={
+          isDark
+            ? "/assets/images/todaysActivity2.png"
+            : "/assets/images/todaysActivity.png"
+        }
+        alt={"today's activity dashboard"}
+        width={686}
+        height={487}
+        style={{ opacity: 1 }}
+      />
+    </div>
+  );
+};
 
-export default TodaysWorkIMG
+export default TodaysWorkIMG;
