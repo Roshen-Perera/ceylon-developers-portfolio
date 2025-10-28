0859681ce8f056c027055e78ef2865c6af0d431d
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-28 14:49:54 +0530

    Refactor NotFoundIMG component for improved readability and consistency

--- components/notFoundIMG.tsx
+++ components/notFoundIMG.tsx
@@ -1,29 +1,41 @@
-import React, { useEffect, useState } from 'react'
-import Image from 'next/image';
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
 
 const NotFoundIMG = () => {
   const [isDark, setIsDark] = useState(false);
-  
-    useEffect(() => {
-      // Check if 'dark' class exists on html or body element
-      const checkDarkMode = () => {
-        setIsDark(document.documentElement.classList.contains("dark"));
-      };
-  
-      checkDarkMode();
-  
-      // Optional: Listen for theme changes
-      const observer = new MutationObserver(checkDarkMode); // Observe changes to the class attribute
-      observer.observe(document.documentElement, {
-        attributes: true, // Listen for attribute changes
-        attributeFilter: ["class"], // Only listen for changes to the 'class' attribute
-      });
-  
-      return () => observer.disconnect(); // Cleanup on unmount
-    }, []);
-  return (
-    <div>NotFoundIMG</div>
-  )
-}
 
-export default NotFoundIMG
+  useEffect(() => {
+    // Check if 'dark' class exists on html or body element
+    const checkDarkMode = () => {
+      setIsDark(document.documentElement.classList.contains("dark"));
+    };
+
+    checkDarkMode();
+
+    // Optional: Listen for theme changes
+    const observer = new MutationObserver(checkDarkMode); // Observe changes to the class attribute
+    observer.observe(document.documentElement, {
+      attributes: true, // Listen for attribute changes
+      attributeFilter: ["class"], // Only listen for changes to the 'class' attribute
+    });
+
+    return () => observer.disconnect(); // Cleanup on unmount
+  }, []);
+  return (
+    <div>
+      <Image
+        src={
+          isDark
+            ? "/assets/images/4042.png"
+            : "/assets/images/404.png"
+        }
+        alt={"today's activity dashboard"}
+        width={686}
+        height={487}
+        style={{ opacity: 1 }}
+      />
+    </div>
+  );
+};
+
+export default NotFoundIMG;
