2392ba362c5f7cb5a5fc07871aa53227c2fd0909
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-28 17:25:31 +0530

    Refactor NavBarPortfolio component for improved layout and consistency

--- components/navBarPortfolio.tsx
+++ components/navBarPortfolio.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import ThemeToggle from "./theme";
 
 const NavBarPortfolio = () => {
   return (
     <div className="flex items-center justify-center w-full p-8 relative z-30">
       <div>
-        <div className="flex flex-row fixed justify-between w-[1140px] h-16 items-center rounded-full px-6 text-black bg-white shadow-lg left-1/2 transform -translate-x-1/2">
+        <div className="flex flex-row fixed justify-between w-[1140px] h-16 items-center rounded-full bg-[var(--navbar-background)] border shadow-lg left-1/2 transform -translate-x-1/2">
           <div className="flex items-center">
             <div>
               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
@@ -22,15 +23,22 @@ const NavBarPortfolio = () => {
               Skills
             </a>
             <a href="#" className="hover:underline transition-all duration-200">
-              Achievements 
+              Achievements
             </a>
             <a href="#" className="hover:underline transition-all duration-200">
               Contact
             </a>
           </div>
-          <button className="h-12 bg-gradient-to-b from-white to-gray-100 hover:from-gray-50 hover:to-gray-200 text-black font-medium py-2 px-6 rounded-full border border-gray-200 transition-all duration-200 ml-8">
-            Github Profile
-          </button>
+          <div className="flex items-center">
+            <div>
+              <ThemeToggle />
+            </div>
+            <div className="p-[1px] rounded-[12px] bg-[var(--navbar-button-border)]">
+              <button className="h-12 bg-gradient-to-b from-white to-gray-100 hover:from-gray-50 hover:to-gray-200 text-black font-medium py-2 px-6 rounded-full border border-gray-200 transition-all duration-200 ml-8">
+                Github Profile
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
