733f66af145bd1698f34ed1f5c1d9bf36a3ae18e
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-28 14:01:26 +0530

    Update ProjectMembers component to use CSS variable for background styling

--- components/projectMembers.tsx
+++ components/projectMembers.tsx
@@ -6,7 +6,7 @@ const ProjectMembers = () => {
     <div>
       <h1 className="text-7xl font-medium">Team Members Involved</h1>
       <div className="flex flex-row gap-10 mt-10">
-        <div className="flex flex-col items-center bg-[#F7F7F7] p-5 rounded-2xl">
+        <div className="flex flex-col items-center bg-[var(--member-card-bg)] p-5 rounded-2xl">
           <Image
             src={"/assets/images/michealr.png"}
             alt="michealr"
@@ -16,7 +16,7 @@ const ProjectMembers = () => {
           <h5 className="text-[30px] font-bold mt-3">Michael R</h5>
           <p className="22px text-[22px] text-[#5C5C5C]">Software Developer</p>
         </div>
-        <div className="flex flex-col items-center bg-[#F7F7F7] p-5 rounded-2xl">
+        <div className="flex flex-col items-center bg-[var(--member-card-bg)] p-5 rounded-2xl">
           <Image
             src={"/assets/images/michealr.png"}
             alt="michealr"
@@ -26,7 +26,7 @@ const ProjectMembers = () => {
           <h5 className="text-[30px] font-bold mt-3">Michael R</h5>
           <p className="22px text-[22px] text-[#5C5C5C]">Software Developer</p>
         </div>
-        <div className="flex flex-col items-center bg-[#F7F7F7] p-5 rounded-2xl">
+        <div className="flex flex-col items-center bg-[var(--member-card-bg)] p-5 rounded-2xl">
           <Image
             src={"/assets/images/michealr.png"}
             alt="michealr"
