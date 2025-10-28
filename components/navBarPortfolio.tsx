d641c79c280c631518cabc68d3633f99e29d61d9
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-28 17:32:11 +0530

    Update NavBarPortfolio component dimensions and text size for consistency

--- components/navBarPortfolio.tsx
+++ components/navBarPortfolio.tsx
@@ -5,14 +5,14 @@ const NavBarPortfolio = () => {
   return (
     <div className="flex items-center justify-center w-full p-8 relative z-30">
       <div>
-        <div className="flex flex-row fixed justify-between w-[873px] h-[43px] items-center rounded-full bg-[var(--navbar-background)] border shadow-lg left-1/2 transform -translate-x-1/2">
+        <div className="flex flex-row fixed justify-between w-[1140px] h-16 items-center rounded-full px-6 text-black bg-white shadow-lg left-1/2 transform -translate-x-1/2">
           <div className="flex items-center">
             <div>
               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
               &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
             </div>
           </div>
-          <div className="flex space-x-6 text-[18px]">
+          <div className="flex space-x-6 text-lg">
             <a href="#" className="hover:underline transition-all duration-200">
               About
             </a>
@@ -34,7 +34,7 @@ const NavBarPortfolio = () => {
               <ThemeToggle />
             </div>
             <div className="p-[1px] rounded-[12px] bg-[var(--navbar-button-border)]">
-              <button className="h-[43px] bg-gradient-to-b from-white to-gray-100 hover:from-gray-50 hover:to-gray-200 text-black font-medium py-2 px-6 rounded-full border border-gray-200 transition-all duration-200 ml-8">
+              <button className="h-12 bg-gradient-to-b from-white to-gray-100 hover:from-gray-50 hover:to-gray-200 text-black font-medium py-2 px-6 rounded-full border border-gray-200 transition-all duration-200 ml-8">
                 Github Profile
               </button>
             </div>
