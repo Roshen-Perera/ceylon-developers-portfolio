dd02514387372e491a0f806bb2107950e8cbda1d
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-29 11:56:43 +0530

    Added Gauze vector image

---
+++ components/gauze.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Gauze = () => {
+  return (
+    <div>
+      <Image src="/assets/vector/gauze.jpeg" alt="gauze" width={1440} height={549} />
+    </div>
+  );
+}
+
+export default Gauze
