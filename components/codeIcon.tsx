7b039936e355c25e59c9810323758a721b4f76ac
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-30 12:11:15 +0530

    Add SVG graphics to CodeIcon component for enhanced visual representation

--- components/codeIcon.tsx
+++ components/codeIcon.tsx
@@ -2,8 +2,207 @@ import React from 'react'
 
 const CodeIcon = () => {
   return (
-    <div>CodeIcon</div>
-  )
+    <div>
+      <svg
+        width="299"
+        height="217"
+        viewBox="0 0 299 217"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <rect
+          x="54.2344"
+          y="59.104"
+          width="220.095"
+          height="96.058"
+          rx="48.029"
+          stroke="#17CDCA"
+          strokeOpacity="0.25"
+          strokeWidth="3"
+        />
+        <rect
+          x="31.708"
+          y="41.3091"
+          width="265.147"
+          height="131.648"
+          rx="65.8238"
+          stroke="url(#paint0_linear_367_71)"
+          strokeOpacity="0.1"
+          strokeWidth="3"
+        />
+        <rect
+          x="88.3311"
+          y="76.0161"
+          width="151.902"
+          height="62.2334"
+          rx="31.1167"
+          fill="url(#paint1_linear_367_71)"
+          fillOpacity="0.28"
+        />
+        <rect
+          x="90.3311"
+          y="78.0161"
+          width="147.902"
+          height="58.2334"
+          rx="29.1167"
+          stroke="url(#paint2_linear_367_71)"
+          strokeOpacity="0.52"
+          strokeWidth="4"
+        />
+        <g filter="url(#filter0_d_367_71)">
+          <path
+            d="M122.136 112.378V105.664L148.991 93.8385V101.563L130.604 108.945L130.852 108.544V109.498L130.604 109.097L148.991 116.479V124.204L122.136 112.378Z"
+            fill="url(#paint3_linear_367_71)"
+          />
+          <path
+            d="M174.073 83.7485L161.485 130.517H154.485L167.073 83.7485H174.073Z"
+            fill="url(#paint4_linear_367_71)"
+          />
+          <path
+            d="M206.427 112.378L179.571 124.204V116.479L197.958 109.097L197.71 109.498V108.544L197.958 108.945L179.571 101.563V93.8385L206.427 105.664V112.378Z"
+            fill="url(#paint5_linear_367_71)"
+          />
+        </g>
+        <path
+          d="M125.085 196.271L193.176 185.643C199.116 184.716 205.067 187.237 208.534 192.148C212.219 197.369 218.684 199.858 224.919 198.458L288.692 184.13"
+          stroke="url(#paint6_linear_367_71)"
+          strokeOpacity="0.25"
+        />
+        <path
+          d="M0.0771484 28.6045L68.1683 17.9773C74.1077 17.0503 80.0592 19.5711 83.5258 24.4822C87.211 29.7029 93.6763 32.1924 99.9112 30.7916L163.684 16.4637"
+          stroke="url(#paint7_linear_367_71)"
+          strokeOpacity="0.25"
+        />
+        <defs>
+          <filter
+            id="filter0_d_367_71"
+            x="118.136"
+            y="83.7485"
+            width="92.291"
+            height="54.7686"
+            filterUnits="userSpaceOnUse"
+            colorInterpolationFilters="sRGB"
+          >
+            <feFlood floodOpacity="0" result="BackgroundImageFix" />
+            <feColorMatrix
+              in="SourceAlpha"
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"
+              result="hardAlpha"
+            />
+            <feOffset dy="4" />
+            <feGaussianBlur stdDeviation="2" />
+            <feComposite in2="hardAlpha" operator="out" />
+            <feColorMatrix
+              type="matrix"
+              values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"
+            />
+            <feBlend
+              mode="normal"
+              in2="BackgroundImageFix"
+              result="effect1_dropShadow_367_71"
+            />
+            <feBlend
+              mode="normal"
+              in="SourceGraphic"
+              in2="effect1_dropShadow_367_71"
+              result="shape"
+            />
+          </filter>
+          <linearGradient
+            id="paint0_linear_367_71"
+            x1="164.282"
+            y1="39.8091"
+            x2="164.282"
+            y2="174.457"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#17CDCA" />
+            <stop offset="1" stopColor="#129795" />
+          </linearGradient>
+          <linearGradient
+            id="paint1_linear_367_71"
+            x1="164.282"
+            y1="138.25"
+            x2="164.282"
+            y2="76.0161"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#080808" />
+            <stop offset="1" stopColor="#17CDCA" />
+          </linearGradient>
+          <linearGradient
+            id="paint2_linear_367_71"
+            x1="164.282"
+            y1="76.0161"
+            x2="164.282"
+            y2="138.25"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#17CDCA" />
+            <stop offset="1" stopColor="#16A6A3" />
+          </linearGradient>
+          <linearGradient
+            id="paint3_linear_367_71"
+            x1="164.308"
+            y1="137.533"
+            x2="167.542"
+            y2="82.7826"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#17CDCA" />
+            <stop offset="1" />
+          </linearGradient>
+          <linearGradient
+            id="paint4_linear_367_71"
+            x1="164.308"
+            y1="137.533"
+            x2="167.542"
+            y2="82.7826"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#17CDCA" />
+            <stop offset="1" />
+          </linearGradient>
+          <linearGradient
+            id="paint5_linear_367_71"
+            x1="164.308"
+            y1="137.533"
+            x2="167.542"
+            y2="82.7826"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="#17CDCA" />
+            <stop offset="1" />
+          </linearGradient>
+          <linearGradient
+            id="paint6_linear_367_71"
+            x1="126.849"
+            y1="206.177"
+            x2="287.525"
+            y2="177.572"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="white" stopOpacity="0" />
+            <stop offset="0.46" stopColor="white" />
+            <stop offset="0.96" stopColor="#999999" stopOpacity="0" />
+          </linearGradient>
+          <linearGradient
+            id="paint7_linear_367_71"
+            x1="1.84071"
+            y1="38.5108"
+            x2="162.517"
+            y2="9.90637"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stopColor="white" stopOpacity="0" />
+            <stop offset="0.46" stopColor="white" />
+            <stop offset="0.96" stopColor="#999999" stopOpacity="0" />
+          </linearGradient>
+        </defs>
+      </svg>
+    </div>
+  );
 }
 
 export default CodeIcon
