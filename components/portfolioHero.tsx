862b62a0a1c2b39ab1c41c20d1d635050f5061bd
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-31 10:53:48 +0530

    Refactor PortfolioHero component for improved readability and structure

--- components/portfolioHero.tsx
+++ components/portfolioHero.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
-import Image from 'next/image'
-import { Download } from 'lucide-react'
+import React from "react";
+import Image from "next/image";
+import { Download } from "lucide-react";
 
 const PortfolioHero = () => {
   return (
@@ -19,59 +19,73 @@ const PortfolioHero = () => {
           </button>
         </div>
       </div>
-      <div className="grid grid-cols-3 mt-20">
-        <div>
-          <svg
-            width="36"
-            height="36"
-            viewBox="0 0 36 36"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M12.135 17.4452H5.1C5.22 10.4402 6.6 9.2852 10.905 6.7352C11.4 6.4352 11.565 5.8052 11.265 5.2952C10.98 4.8002 10.335 4.6352 9.84 4.9352C4.77 7.9352 3 9.7652 3 18.4802V26.5652C3 29.1302 5.085 31.2002 7.635 31.2002H12.135C14.775 31.2002 16.77 29.2052 16.77 26.5652V22.0652C16.77 19.4402 14.775 17.4452 12.135 17.4452Z"
-              fill="var(--feature-text)"
-            />
-            <path
-              d="M28.3651 17.4452H21.3301C21.4501 10.4402 22.8301 9.2852 27.1351 6.7352C27.6301 6.4352 27.7951 5.8052 27.4951 5.2952C27.1951 4.8002 26.5651 4.6352 26.0551 4.9352C20.9851 7.9352 19.2151 9.7652 19.2151 18.4952V26.5802C19.2151 29.1452 21.3001 31.2152 23.8501 31.2152H28.3501C30.9901 31.2152 32.9851 29.2202 32.9851 26.5802V22.0802C33.0001 19.4402 31.0051 17.4452 28.3651 17.4452Z"
-              fill="var(--feature-text)"
-            />
-          </svg>
-
-          <p className="text-[22px] mt-4">
-            Jenny’s Exceptional UI design ensure our website’s success.Highly
-            Recommended
-          </p>
-        </div>
-        <div className="custom-eclipse">
+      <div className="relative">
+        {/* Background Image - Behind and positioned above */}
+        <div className="absolute inset-0 z-0 -top-10">
           <Image
-            src="/assets/images/girl.png"
-            alt="Jenny"
-            width={487}
-            height={636}
-            className="relative bottom-17"
+            src="/assets/vectors/vector.png"
+            alt="Hero Image"
+            width={1439}
+            height={775}
           />
         </div>
 
-        <div>
-          <div className="relative z-10 text-right w-full pr-8">
-            {/* Rating Stars */}
-            <div className="flex gap-1 justify-end mb-2">
-              {[...Array(5)].map((_, i) => (
-                <svg
-                  key={i}
-                  className="w-5 h-5 fill-yellow-400"
-                  viewBox="0 0 24 24"
-                >
-                  <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z" />
-                </svg>
-              ))}
-            </div>
+        {/* Content Grid - In Front */}
+        <div className="grid grid-cols-3 mt-20 relative z-10">
+          <div>
+            <svg
+              width="36"
+              height="36"
+              viewBox="0 0 36 36"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M12.135 17.4452H5.1C5.22 10.4402 6.6 9.2852 10.905 6.7352C11.4 6.4352 11.565 5.8052 11.265 5.2952C10.98 4.8002 10.335 4.6352 9.84 4.9352C4.77 7.9352 3 9.7652 3 18.4802V26.5652C3 29.1302 5.085 31.2002 7.635 31.2002H12.135C14.775 31.2002 16.77 29.2052 16.77 26.5652V22.0652C16.77 19.4402 14.775 17.4452 12.135 17.4452Z"
+                fill="var(--feature-text)"
+              />
+              <path
+                d="M28.3651 17.4452H21.3301C21.4501 10.4402 22.8301 9.2852 27.1351 6.7352C27.6301 6.4352 27.7951 5.8052 27.4951 5.2952C27.1951 4.8002 26.5651 4.6352 26.0551 4.9352C20.9851 7.9352 19.2151 9.7652 19.2151 18.4952V26.5802C19.2151 29.1452 21.3001 31.2152 23.8501 31.2152H28.3501C30.9901 31.2152 32.9851 29.2202 32.9851 26.5802V22.0802C33.0001 19.4402 31.0051 17.4452 28.3651 17.4452Z"
+                fill="var(--feature-text)"
+              />
+            </svg>
 
-            {/* Experience Badge */}
-            <div className="text-right">
-              <div className="text-[50px] font-bold ">5 Years</div>
-              <div className="text-[22px]">Experience</div>
+            <p className="text-[22px] mt-4">
+              Jenny's Exceptional UI design ensure our website's success.Highly
+              Recommended
+            </p>
+          </div>
+
+          <div className="custom-eclipse">
+            <Image
+              src="/assets/images/girl.png"
+              alt="Jenny"
+              width={487}
+              height={636}
+              className="relative bottom-17"
+            />
+          </div>
+
+          <div>
+            <div className="relative z-10 text-right w-full pr-8">
+              {/* Rating Stars */}
+              <div className="flex gap-1 justify-end mb-2">
+                {[...Array(5)].map((_, i) => (
+                  <svg
+                    key={i}
+                    className="w-5 h-5 fill-yellow-400"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z" />
+                  </svg>
+                ))}
+              </div>
+
+              {/* Experience Badge */}
+              <div className="text-right">
+                <div className="text-[50px] font-bold ">5 Years</div>
+                <div className="text-[22px]">Experience</div>
+              </div>
             </div>
           </div>
         </div>
@@ -87,6 +101,6 @@ const PortfolioHero = () => {
       </p>
     </div>
   );
-}
+};
 
-export default PortfolioHero
+export default PortfolioHero;
