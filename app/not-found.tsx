d8239f0d241543969d4f6c6eb18bc4665161f40d
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-10-31 16:07:56 +0530

    Add NotFound page component with layout and navigation

---
+++ app/not-found.tsx
@@ -0,0 +1,37 @@
+import BlueGlow from "@/components/blueGlow";
+import BookACallPost from "@/components/bookACallPost";
+import Footer from "@/components/footer";
+import NavBar from "@/components/navBar";
+import NotFoundIMG from "@/components/notFoundIMG";
+import React from "react";
+
+const Page = () => {
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <BlueGlow />
+      <NavBar />
+      <div className="mt-20">
+        <NotFoundIMG />
+      </div>
+      <div className="text-lg mt-10 max-w-2xl flex items-center mx-auto px-4">
+        <p className="text-[22px] text-center">
+          We’re sorry. the page you requested could no be found Please go back
+          to the home page
+        </p>
+      </div>
+      <div className="mt-8 flex max-w-4xl gap-10 justify-center items-center">
+        <button className="text-[22px] p-3 text-lg bg-[#17CDCA] rounded-2xl">
+          Back to Home
+        </button>
+      </div>
+      <div className="mt-20 mb-12">
+        <BookACallPost />
+      </div>
+      <footer className="py-12 mt-12">
+        <Footer />
+      </footer>
+    </div>
+  );
+};
+
+export default Page;
