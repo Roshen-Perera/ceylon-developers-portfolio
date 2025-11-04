4bfd4b4c66cf05329012c25abffff59805d63a37
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-11-04 18:28:58 +0530

    Update ThemeToggle button SVG dimensions for enhanced responsiveness across various screen sizes

--- components/theme.tsx
+++ components/theme.tsx
@@ -64,7 +64,7 @@ export default function ThemeToggle() {
               className="absolute"
             >
               <svg
-                className="w-[24px] h-[24px] md:w-[32px] md:h-[32px] lg:w-[40px] lg:h-[40px]"
+                className="xs:w-5 xs:h-5 sm:w-[27.19px] sm:h-[27.19px] md:w-8 md:h-8 lg:w-[35.62499237060547px] lg:h-[35.62499237060547px] xl:w-10 xl:h-10 2xl:w-10 2xl:h-10"
                 viewBox="0 0 40 40"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
@@ -85,7 +85,7 @@ export default function ThemeToggle() {
               className="absolute"
             >
               <svg
-                className="xs:w-5 xs:h-5 sm:w-[27.19px] sm:h-[27.19px] md:w-8 md:h-8 lg:w-[35.62499237060547px] lg:h-[35.62499237060547px] xl:w-[40px] xl:h-[40px] 2xl:w-10 2xl:h-10"
+                className="xs:w-5 xs:h-5 sm:w-[27.19px] sm:h-[27.19px] md:w-8 md:h-8 lg:w-[35.62499237060547px] lg:h-[35.62499237060547px] xl:w-10 xl:h-10 2xl:w-10 2xl:h-10"
                 viewBox="0 0 30 30"
                 fill="none"
                 xmlns="http://www.w3.org/2000/svg"
