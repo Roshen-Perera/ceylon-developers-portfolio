aa0fae5b6f020134e41f8fdae89d37e2a88ea0fb
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-11-06 18:17:47 +0530

    Refactor Services component layout for improved semantic structure and consistency

--- components/services.tsx
+++ components/services.tsx
@@ -1,26 +1,28 @@
-import React from 'react'
-import ServiceCarousel from './serviceCarousel';
-import { H2, P } from './typography';
+import React from "react";
+import ServiceCarousel from "./serviceCarousel";
+import { H2, P } from "./typography";
 
 const Services = () => {
   return (
     <>
       <div className="flex flex-col items-center justify-center mt-12">
-        <H2 className="text-7xl font-medium  md:max-w-full max-w-sm">
-          <span className="font-kaushan">Services</span> we offer{" "}
-        </H2>
-        <div className="mt-8 max-w-2xl flex items-center mx-auto px-4">
+        <div className="sm:max-w-full max-w-sm">
+          <H2 className="text-7xl font-medium  ">
+            <span className="font-kaushan">Services</span> we offer
+          </H2>
+        </div>
+        <div className="lg:mt-8 mt-4 lg:max-w-2xl md:max-w-xl max-w-xs flex items-center">
           <P className="text-center">
             Pushing the boundaries of what&apos;s possible in the digital realm.
             Join us on this journey into the future of technology.
           </P>
         </div>
-        <div className="mt-8">
+        {/* <div className="mt-8">
           <ServiceCarousel />
-        </div>
+        </div> */}
       </div>
     </>
   );
-}
+};
 
-export default Services
+export default Services;
