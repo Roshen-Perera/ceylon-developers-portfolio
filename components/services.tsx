6d242762216ad0dd8a1ea4490d6ba1319c06956f
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-11-06 18:12:50 +0530

    Replace paragraph with H2 component for improved semantic structure in Services section

--- components/services.tsx
+++ components/services.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import ServiceCarousel from './serviceCarousel';
+import { H2 } from './typography';
 
 const Services = () => {
   return (
     <>
       <div className="flex flex-col items-center justify-center mt-12">
-        <p className="text-7xl font-medium">
+        <H2 className="text-7xl font-medium">
           <span className="font-kaushan">Services</span> we offer{" "}
-        </p>
-        <div className="text-[22px] mt-8 max-w-2xl flex items-center mx-auto px-4">
+        </H2>
+        <div className="mt-8 max-w-2xl flex items-center mx-auto px-4">
           <p className="text-center">
             Pushing the boundaries of what&apos;s possible in the digital realm.
             Join us on this journey into the future of technology.
