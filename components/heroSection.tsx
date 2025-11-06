b0e3343b8dfb858f86ac73efc186aac2dd7b0a20
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-11-06 11:47:17 +0530

    Update HeroSection layout for improved responsiveness and alignment

--- components/heroSection.tsx
+++ components/heroSection.tsx
@@ -15,7 +15,7 @@ const HeroSection = () => {
         </H1>
         <H1 className="font-semibold text-8xl items-center">Excellence</H1>
       </div>
-      <div className="mt-8 max-w-2xl flex items-center mx-auto px-4">
+      <div className="mt-8 lg:max-w-2xl sm:max-w-xs flex items-center">
         <P className="text-center">
           Pushing the boundaries of what&apos;s possible in the digital realm.
           Join us on this journey into the future of technology.
