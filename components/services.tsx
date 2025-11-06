53b2a0ed05734c51b5979eb1ec4bde10b63248e4
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-11-06 18:14:57 +0530

    Refactor Services component to improve semantic structure and consistency by importing P component and adjusting H2 properties

--- components/services.tsx
+++ components/services.tsx
@@ -1,19 +1,19 @@
 import React from 'react'
 import ServiceCarousel from './serviceCarousel';
-import { H2 } from './typography';
+import { H2, P } from './typography';
 
 const Services = () => {
   return (
     <>
       <div className="flex flex-col items-center justify-center mt-12">
-        <H2 className="text-7xl font-medium">
+        <H2 className="text-7xl font-medium  md:max-w-full max-w-sm">
           <span className="font-kaushan">Services</span> we offer{" "}
         </H2>
         <div className="mt-8 max-w-2xl flex items-center mx-auto px-4">
-          <p className="text-center">
+          <P className="text-center">
             Pushing the boundaries of what&apos;s possible in the digital realm.
             Join us on this journey into the future of technology.
-          </p>
+          </P>
         </div>
         <div className="mt-8">
           <ServiceCarousel />
