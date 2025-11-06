01d5ae14b776000acee4fcb0e54a09f9b03caac1
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-11-06 13:46:21 +0530

    Add LogoIMG component to handle dark mode logo switching

---
+++ components/logoIMG.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
+
+const LogoIMG = () => {
+  const [isDark, setIsDark] = useState(false);
+
+  useEffect(() => {
+    // Check if 'dark' class exists on html or body element
+    const checkDarkMode = () => {
+      setIsDark(document.documentElement.classList.contains("dark"));
+    };
+
+    checkDarkMode();
+
+    // Optional: Listen for theme changes
+    const observer = new MutationObserver(checkDarkMode); // Observe changes to the class attribute
+    observer.observe(document.documentElement, {
+      attributes: true, // Listen for attribute changes
+      attributeFilter: ["class"], // Only listen for changes to the 'class' attribute
+    });
+
+    return () => observer.disconnect(); // Cleanup on unmount
+  }, []);
+  return (
+    <div>
+      <Image
+        src={isDark ? "/assets/icons/logo.png" : "/assets/icons/logo2.png"}
+        alt={"today's activity dashboard"}
+        width={435.00439453125}
+        height={354}
+        style={{ opacity: 1 }}
+      />
+    </div>
+  );
+};
+
+export default LogoIMG;
