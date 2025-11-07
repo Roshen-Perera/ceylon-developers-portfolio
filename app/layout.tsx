f405182bcdaf371f3ad3384dfba27ba2e919d30f
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-11-07 20:47:09 +0530

    Remove unused Embla CSS import from layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 // app/layout.tsx
 import { Inter, Kaushan_Script } from "next/font/google";
 import "./globals.css";
-import "../css/embla.css";
 
 // Default font for whole app
 const inter = Inter({
