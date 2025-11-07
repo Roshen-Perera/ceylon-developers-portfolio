7aa2929a1bcd39f92a960cc0f1e468e4c3b4cdea
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-11-07 18:47:56 +0530

    Refactor ServiceCarousel component structure for improved readability and maintainability

--- components/serviceCarousel.tsx
+++ components/serviceCarousel.tsx
@@ -34,78 +34,80 @@ const ServiceCarousel: React.FC<PropType> = (props) => {
     onNavButtonClick
   );
 
-    const services = [
-      {
-        title: "Review",
-        description:
-          "A Website is an extension of yourself and we can help you to express it properly. Your website is your number one marketing asset because we live in a digital age.",
-        icon: "/assets/icons/review.jpg",
-      },
-      {
-        title: "Software Testing Service",
-        description:
-          "A Website is an extension of yourself and we can help you to express it properly. Your website is your number one marketing asset because we live in a digital age.",
-        icon: "/assets/icons/testing.gif",
-      },
-      {
-        title: "Web Design & Development",
-        description:
-          "A Website is an extension of yourself and we can help you to express it properly. Your website is your number one marketing asset because we live in a digital age.",
-        icon: "/assets/icons/web.gif",
-      },
-      {
-        title: "Mobile App Development",
-        description:
-          "A Website is an extension of yourself and we can help you to express it properly. Your website is your number one marketing asset because we live in a digital age.",
-        icon: "/assets/icons/mobile.gif",
-      },
-      {
-        title: "UI/UX Design",
-        description:
-          "A Website is an extension of yourself and we can help you to express it properly. Your website is your number one marketing asset because we live in a digital age.",
-        icon: "/assets/icons/ui.gif",
-      },
-    ];
+  const services = [
+    {
+      title: "Review",
+      description:
+        "A Website is an extension of yourself and we can help you to express it properly. Your website is your number one marketing asset because we live in a digital age.",
+      icon: "/assets/icons/review.jpg",
+    },
+    {
+      title: "Software Testing Service",
+      description:
+        "A Website is an extension of yourself and we can help you to express it properly. Your website is your number one marketing asset because we live in a digital age.",
+      icon: "/assets/icons/testing.gif",
+    },
+    {
+      title: "Web Design & Development",
+      description:
+        "A Website is an extension of yourself and we can help you to express it properly. Your website is your number one marketing asset because we live in a digital age.",
+      icon: "/assets/icons/web.gif",
+    },
+    {
+      title: "Mobile App Development",
+      description:
+        "A Website is an extension of yourself and we can help you to express it properly. Your website is your number one marketing asset because we live in a digital age.",
+      icon: "/assets/icons/mobile.gif",
+    },
+    {
+      title: "UI/UX Design",
+      description:
+        "A Website is an extension of yourself and we can help you to express it properly. Your website is your number one marketing asset because we live in a digital age.",
+      icon: "/assets/icons/ui.gif",
+    },
+  ];
 
   return (
-    <section className="embla">
-      <div className="embla__viewport" ref={emblaRef}>
-        <div className="embla__container">
-          {services.map((service, index) => (
-            <div className="embla__slide" key={index}>
-              <div
-                className="flex flex-col justify border-[#17CDCA] border rounded-lg p-3 gap-4"
-              >
-                <Image
-                  src={service.icon}
-                  alt={service.title}
-                  width={40}
-                  height={40}
-                />
-                <BodySemi className="text-[26px] font-semibold text-[#17CDCA]">
-                  {service.title}
-                </BodySemi>
-                <P>{service.description}</P>
+    <div className="w-full overflow-hidden">
+      {" "}
+      {/* Added wrapper */}
+      <section className="embla">
+        <div className="embla__viewport" ref={emblaRef}>
+          <div className="embla__container">
+            {services.map((service, index) => (
+              <div className="embla__slide" key={index}>
+                <div className="flex flex-col justify border-[#17CDCA] border rounded-lg p-3 gap-4 h-full">
+                  <Image
+                    src={service.icon}
+                    alt={service.title}
+                    width={40}
+                    height={40}
+                  />
+                  <BodySemi className="text-[26px] font-semibold text-[#17CDCA]">
+                    {service.title}
+                  </BodySemi>
+                  <P>{service.description}</P>
+                </div>
               </div>
-            </div>
-          ))}
+            ))}
+          </div>
         </div>
-      </div>
 
-      <div className="embla__controls">
-        <div className="embla__dots">
-          {scrollSnaps.map((_, index) => (
-            <DotButton
-              key={index}
-              onClick={() => onDotButtonClick(index)}
-              className={"embla__dot".concat(
-                index === selectedIndex ? " embla__dot--selected" : ""
-              )}
-            />
-          ))}
+        <div className="embla__controls">
+          <div className="embla__dots">
+            {scrollSnaps.map((_, index) => (
+              <DotButton
+                key={index}
+                onClick={() => onDotButtonClick(index)}
+                className={"embla__dot".concat(
+                  index === selectedIndex ? " embla__dot--selected" : ""
+                )}
+              />
+            ))}
+          </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
 
