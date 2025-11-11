36269952d130fadc42a982c102eaee33e00175ba
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-11-11 18:39:00 +0530

    Refactor WeDoSection component to enhance layout structure and improve consistency in typography

--- components/weDoSection.tsx
+++ components/weDoSection.tsx
@@ -6,47 +6,55 @@ import { H5, P } from './typography';
 const WeDoSection = () => {
   return (
     <div className="flex flex-col items-center justify-center mt-12">
-      <div className="grid grid-cols-1 lg:grid-cols-2 ml-15 mr-15 gap-5 mt-8">
-        <div className="flex flex-col justify-around">
+      <div className="grid grid-cols-1 lg:grid-cols-[auto_1fr] ml-15 mr-15 gap-5 mt-8">
+        <div className="flex flex-col justify-around max-w-md lg:max-w-sm xl:max-w-md">
           <VerticalTextSlider />
-          <div className=" flex items-center">
+          <div className="flex items-center">
             <P>
               Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
               nonumy eirmod tempor invidunt ut labore
             </P>
           </div>
         </div>
-        <div className="bg-(--wedo-bg) bg-(image:--wedo-bg) flex flex-col vision-border p-6 text-center gap-4">
-          <H5 className="text-[26px] font-bold">Consulting</H5>
-          <P className=" text-(--wedo-text)">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry&apos;s standard
-          </P>
-          <div className="flex flex-row gap-2 items-center text-[#17CDCA] ">
-            <P>Get Started</P>
-            <MoveRight />
+        <div className="flex flex-col sm:flex-row gap-3 min-w-0">
+          <div className="bg-(--wedo-bg) bg-(image:--wedo-bg) flex flex-col vision-border p-4 sm:p-5 md:p-6 text-center gap-3 md:gap-4 flex-1">
+            <H5 className="text-xl sm:text-2xl md:text-[26px] font-bold">
+              Consulting
+            </H5>
+            <P className="text-(--wedo-text) text-sm sm:text-base">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry&apos;s standard
+            </P>
+            <div className="flex flex-row gap-2 items-center justify-center text-[#17CDCA]">
+              <P>Get Started</P>
+              <MoveRight />
+            </div>
           </div>
-        </div>
-        <div className="bg-(--wedo-bg) bg-(image:--wedo-bg) flex flex-col vision-border-middle p-6 text-center gap-4">
-          <H5 className="text-[26px] font-bold">Implementation</H5>
-          <P className=" text-(--wedo-text)">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry&apos;s standard
-          </P>
-          <div className="flex flex-row gap-2 items-center text-[#17CDCA] ">
-            <P>Get Started</P>
-            <MoveRight />
+          <div className="bg-(--wedo-bg) bg-(image:--wedo-bg) flex flex-col vision-border-middle p-4 sm:p-5 md:p-6 text-center gap-3 md:gap-4 flex-1">
+            <H5 className="text-xl sm:text-2xl md:text-[26px] font-bold">
+              Implementation
+            </H5>
+            <P className="text-(--wedo-text) text-sm sm:text-base">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry&apos;s standard
+            </P>
+            <div className="flex flex-row gap-2 items-center justify-center text-[#17CDCA]">
+              <P>Get Started</P>
+              <MoveRight />
+            </div>
           </div>
-        </div>
-        <div className="bg-(--wedo-bg) bg-(image:--wedo-bg) flex flex-col vision-border p-6 text-center gap-4">
-          <H5 className="text-[26px] font-bold">Distribution</H5>
-          <P className=" text-(--wedo-text)">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry&apos;s standard
-          </P>
-          <div className="flex flex-row gap-2 items-center text-[#17CDCA] ">
-            <P>Get Started</P>
-            <MoveRight />
+          <div className="bg-(--wedo-bg) bg-(image:--wedo-bg) flex flex-col vision-border p-4 sm:p-5 md:p-6 text-center gap-3 md:gap-4 flex-1">
+            <H5 className="text-xl sm:text-2xl md:text-[26px] font-bold">
+              Distribution
+            </H5>
+            <P className="text-(--wedo-text) text-sm sm:text-base">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry. Lorem Ipsum has been the industry&apos;s standard
+            </P>
+            <div className="flex flex-row gap-2 items-center justify-center text-[#17CDCA]">
+              <P>Get Started</P>
+              <MoveRight />
+            </div>
           </div>
         </div>
       </div>
