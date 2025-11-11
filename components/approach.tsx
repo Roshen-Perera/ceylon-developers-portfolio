7f6cffdb9d5dfb99ccd51f2833f7a5c9f19c7ba3
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-11-11 10:04:55 +0530

    Refactor Approach component to replace paragraph with H2 for improved semantics

--- components/approach.tsx
+++ components/approach.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import { H2 } from './typography';
 
 const Approach = () => {
   return (
     <div className="flex flex-col items-center justify-center mt-12">
       <div className="text-center mb-8">
-        <p className="text-7xl font-medium">
+        <H2 className="text-7xl font-medium">
           Our <span className="font-kaushan">design</span> and
           <br />
           <span className="font-kaushan">development</span> approach
-        </p>
+        </H2>
       </div>
       <div className="grid grid-cols-4 p-8">
         <div className="p-3 shadow-[0px_0px_15px_rgba(23,205,202,0.47)] rounded-lg">
