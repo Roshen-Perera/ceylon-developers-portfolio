750e29543625ae6903f6a0f99905a13cce2dea6b
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-11-11 10:05:39 +0530

    Refactor Approach component to comment out grid layout for future adjustments

--- components/approach.tsx
+++ components/approach.tsx
@@ -11,7 +11,7 @@ const Approach = () => {
           <span className="font-kaushan">development</span> approach
         </H2>
       </div>
-      <div className="grid grid-cols-4 p-8">
+      {/* <div className="grid grid-cols-4 p-8">
         <div className="p-3 shadow-[0px_0px_15px_rgba(23,205,202,0.47)] rounded-lg">
           <div className="flex flex-col p-5 gradient-border rounded-2xl gap-4 ">
             <p className="text-[26px] font-bold">UX Driven Engineering</p>
@@ -82,7 +82,7 @@ const Approach = () => {
           </div>
         </div>
         <div></div>
-      </div>
+      </div> */}
     </div>
   );
 }
