899d847b75d9bdd027d0feec67afedaa6c7ddc89
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-11-14 18:21:16 +0530

    Refactor ProPost component to adjust dimensions for improved responsiveness

--- components/proPost.tsx
+++ components/proPost.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const ProPost = () => {
   return (
     <>
-      <div className="flex flex-col gap-7 p-7 rounded-[20px] items-center w-[984px] h-[409.75px] text-center bg-(--post-bg) bg-(image:--post-bg) blur-(--post-blur)">
+      <div className="flex flex-col gap-7 p-7 rounded-[20px] items-center w-[345px] h-[333px] sm:w-[499px] sm:h-[325px] md:w-[573px] md:h-[353px] lg:w-[779px] lg:h-[355px] xl:w-[913px] xl:h-96 2xl:w-[984px] 2xl:h-[410px] text-center bg-(--post-bg) bg-(image:--post-bg) blur-(--post-blur)">
         <h1 className="font-medium text-7xl">
           Ready to <span className="font-kaushan"> Manage </span> your
           <br />
