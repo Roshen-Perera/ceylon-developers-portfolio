10530f56f3ab68b93136d4cc1817b1ad4cb2eeb5
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-11-14 18:21:30 +0530

    Refactor ProPost component to replace h1 tag with H2 component for improved consistency

--- components/proPost.tsx
+++ components/proPost.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import { H2 } from './typography';
 
 const ProPost = () => {
   return (
     <>
       <div className="flex flex-col gap-7 p-7 rounded-[20px] items-center w-[345px] h-[333px] sm:w-[499px] sm:h-[325px] md:w-[573px] md:h-[353px] lg:w-[779px] lg:h-[355px] xl:w-[913px] xl:h-96 2xl:w-[984px] 2xl:h-[410px] text-center bg-(--post-bg) bg-(image:--post-bg) blur-(--post-blur)">
-        <h1 className="font-medium text-7xl">
+        <H2 className="font-medium text-7xl">
           Ready to <span className="font-kaushan"> Manage </span> your
           <br />
           team like a pro?
-        </h1>
+        </H2>
         <p className="text-[22px]">
           Pushing the boundaries of what&apos;s possible in the digital realm.
           Join us on this journey into the future of technology.
