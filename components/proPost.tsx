fc8e062817d2bb1219a97ec740d57cd6c38d8d29
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-11-14 18:21:37 +0530

    Refactor ProPost component to remove unnecessary class from H2 for cleaner markup

--- components/proPost.tsx
+++ components/proPost.tsx
@@ -5,7 +5,7 @@ const ProPost = () => {
   return (
     <>
       <div className="flex flex-col gap-7 p-7 rounded-[20px] items-center w-[345px] h-[333px] sm:w-[499px] sm:h-[325px] md:w-[573px] md:h-[353px] lg:w-[779px] lg:h-[355px] xl:w-[913px] xl:h-96 2xl:w-[984px] 2xl:h-[410px] text-center bg-(--post-bg) bg-(image:--post-bg) blur-(--post-blur)">
-        <H2 className="font-medium text-7xl">
+        <H2>
           Ready to <span className="font-kaushan"> Manage </span> your
           <br />
           team like a pro?
