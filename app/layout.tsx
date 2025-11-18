dbde60d1bf057750109f85eb90afafc3ccda1ab0
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-11-18 18:09:09 +0530

    Remove dark class from html tag for improved layout consistency

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className="dark">
+    <html lang="en">
       {/* Apply default font globally */}
       <body className={`${inter.className} ${kaushan.variable}`}>
         {children}
