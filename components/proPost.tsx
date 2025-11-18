8e3cd8fed392d06610445566baf2802ab5bba28c
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-11-18 17:39:40 +0530

    Remove fixed height from ProPost component for improved responsiveness

--- components/proPost.tsx
+++ components/proPost.tsx
@@ -4,7 +4,7 @@ import { ButtonText, H2, P } from './typography';
 const ProPost = () => {
   return (
     <>
-      <div className="flex flex-col gap-7 p-7 rounded-[20px] items-center w-[345px] h-[333px] sm:w-[499px] sm:h-[325px] md:w-[573px] md:h-[353px] lg:w-[779px] lg:h-[355px] xl:w-[913px] xl:h-96 2xl:w-[984px] 2xl:h-[410px] text-center bg-(--post-bg) bg-(image:--post-bg) blur-(--post-blur)">
+      <div className="flex flex-col gap-7 p-7 rounded-[20px] items-center w-[345px] sm:w-[499px]  md:w-[573px]  lg:w-[779px]  xl:w-[913px] 2xl:w-[984px] text-center bg-(--post-bg) bg-(image:--post-bg) blur-(--post-blur)">
         <H2>
           Ready to <span className="font-kaushan"> Manage </span> your
           <br />
