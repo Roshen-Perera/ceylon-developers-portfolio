4757848dfc4b3f8c734d406e48c7a5064aebb0f2
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-11-19 14:38:32 +0530

    Refactor Approaches component to include responsive layout divs

--- components/approaches.tsx
+++ components/approaches.tsx
@@ -3,9 +3,14 @@ import React from 'react'
 const Approaches = () => {
   return (
     <>
-        
+      <div className="2xl:block hidden"></div>
+      <div className="xl:block hidden"></div>
+      <div className="lg:block hidden"></div>
+      <div className="md:block hidden"></div>
+      <div className="sm:block hidden"></div>
+      <div className="xs:block hidden"></div>
     </>
-  )
+  );
 }
 
 export default Approaches
