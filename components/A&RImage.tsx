185f43ae0a4549a275b69930e82264cd99f12856
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-11-26 15:43:49 +0530

    Add responsive A&R image with dark mode support

--- components/A&RImage.tsx
+++ components/A&RImage.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
+import Image from "next/image";
 
 const ARImg = () => {
   const [isDark, setIsDark] = useState(false);
@@ -22,8 +23,15 @@ const ARImg = () => {
   }, []);
   return (
     <>
+      <Image
+        src={ isDark ? "/assets/vectors/A&R-dark.png" : "/assets/vectors/A&R.png"}
+        alt={""}
+        width={400}
+        height={323}
+        className="w-48 sm:w-[277px] md:w-[323px] lg:w-[293px] xl:w-[374px] 2xl:w-[400px] h-auto transition-opacity duration-500 hover:opacity-90"
+      />
     </>
-  )
-}
+  );
+};
 
-export default ARImg
+export default ARImg;
