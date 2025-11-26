7a73b580d6d677a4a31ee7bbd36ffcdfb7eb5c40
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-11-26 15:05:26 +0530

    Removed Analytics vector svg

--- components/facilities.tsx
+++ components/facilities.tsx
@@ -47,7 +47,7 @@ const Facilities = () => {
           <div className="2xl:w-[786px] 2xl:h-[375px] xl:w-[735px] xl:h-[350px] lg:w-[577px] lg:h-[329px] bg-[#6F6F6F1A] border rounded-4xl overflow-hidden shrink-0">
             <div className="flex">
               <div className="mt-12 ml-3 shrink-0">
-                <AnalyticsVector />
+                
               </div>
               <div className="flex flex-col gap-2 -ml-37 mt-4 z-20">
                 <H4>Analytics & Reporting</H4>
