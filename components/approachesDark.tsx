db9ac2416e1546d6351185ede16ee14b0cf71b2b
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-11-27 12:48:26 +0530

    Add ApproachesDark component for dark mode support

---
+++ components/approachesDark.tsx
@@ -0,0 +1,17 @@
+
+import React from "react";
+
+const ApproachesDark = () => {
+  return (
+    <div>
+      {/* 1440 */}
+      {/* 1280 */}
+      {/* 1024 */}
+      {/* 768 */}
+      {/* 640 */}
+      {/* 375 */}
+    </div>
+  );
+};
+
+export default ApproachesDark;
