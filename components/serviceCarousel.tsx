4dc2007505369c43169e2fb551ebaf6dad583579
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-05 20:07:59 +0530

    Refactor ServiceCarousel to import Swiper for enhanced carousel functionality

--- components/serviceCarousel.tsx
+++ components/serviceCarousel.tsx
@@ -1,9 +1,9 @@
 "use client";
 
-import React, { useEffect, useState, useCallback, useMemo } from "react";
-import Image from "next/image";
-import { ChevronLeft, ChevronRight } from "lucide-react";
-import { H5, P } from "./typography";
+// import Swiper JS
+import Swiper from 'swiper';
+// import Swiper styles
+import 'swiper/css';
 
 const ServiceCarousel = () => {
 
