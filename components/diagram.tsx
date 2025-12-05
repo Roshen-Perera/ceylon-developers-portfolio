8e85757af0c01551c3a9d429175f824cd899581a
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-05 11:08:20 +0530

    Refactor Diagram component to remove leader-line integration and simplify structure

--- components/diagram.tsx
+++ components/diagram.tsx
@@ -1,63 +1,9 @@
-"use client";
+import React from 'react'
 
-import { useEffect, useRef } from "react";
-import LeaderLine from "leader-line-new";
-
-export default function Diagram() {
-  const card1Ref = useRef<HTMLDivElement>(null);
-  const card2Ref = useRef<HTMLDivElement>(null);
-
-  useEffect(() => {
-    if (card1Ref.current && card2Ref.current) {
-      const line = new LeaderLine(card1Ref.current, card2Ref.current, {
-        color: "#000",
-        size: 3,
-        startPlug: "behind",
-        endPlug: "arrow1",
-        path: "straight",
-      });
-
-      return () => line.remove(); // cleanup on component unmount
-    }
-  }, []);
-
+const Diagram = () => {
   return (
-    <div style={{ position: "relative", width: "100%", height: "500px" }}>
-      <div
-        ref={card1Ref}
-        style={{
-          width: 150,
-          height: 80,
-          background: "#e3e3e3",
-          borderRadius: 10,
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          position: "absolute",
-          top: 100,
-          left: 100,
-        }}
-      >
-        Card 1
-      </div>
-
-      <div
-        ref={card2Ref}
-        style={{
-          width: 150,
-          height: 80,
-          background: "#e3e3e3",
-          borderRadius: 10,
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          position: "absolute",
-          top: 300,
-          left: 300,
-        }}
-      >
-        Card 2
-      </div>
-    </div>
-  );
+    <></>
+  )
 }
+
+export default Diagram
