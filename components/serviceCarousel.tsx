844afdd4010b5a041549dd528c46eef7a736940b
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-08 10:40:51 +0530

    Refactor ServiceCarousel to streamline component structure and enhance Swiper integration

--- components/serviceCarousel.tsx
+++ components/serviceCarousel.tsx
@@ -1,17 +1,34 @@
 "use client";
 
-// import Swiper JS
-import Swiper from 'swiper';
-// import Swiper styles
-import 'swiper/css';
+import React, { useEffect } from "react";
+import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
+import "swiper/css";
 
-const ServiceCarousel = () => {
-const swiper = new Swiper(...);
+export default function MySwiper() {
+  useEffect(() => {
+    const swiper = new Swiper(".swiper", {
+      modules: [Navigation, Pagination],
+      loop: true,
+      pagination: { el: ".swiper-pagination" },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+  }, []);
+
   return (
-    <>
-    
-    </>
-  );
-};
+    <div className="swiper">
+      <div className="swiper-wrapper">
+        <div className="swiper-slide">Slide 1</div>
+        <div className="swiper-slide">Slide 2</div>
+        <div className="swiper-slide">Slide 3</div>
+      </div>
 
-export default ServiceCarousel;
+      <div className="swiper-pagination"></div>
+      <div className="swiper-button-next"></div>
+      <div className="swiper-button-prev"></div>
+    </div>
+  );
+}
