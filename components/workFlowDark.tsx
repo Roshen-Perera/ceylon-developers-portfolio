71d37e6d41faaaa1e475a16894a91f8bee5dcd17
Roshen-Perera <roshenpilishiyanperera@gmail.com>
2025-12-18 14:45:08 +0530

    fix: Update visibility class for SVG in WorkFlowDark for improved responsiveness

--- components/workFlowDark.tsx
+++ components/workFlowDark.tsx
@@ -7,7 +7,7 @@ const WorkFlowDark = () => {
       <svg
         width="551"
         height="229"
-        className=" 2xl:block xl:hidden lg:hidden md:hidden sm:hidden xs:hidden block"
+        className=" 2xl:block xl:hidden lg:hidden md:hidden sm:hidden hidden"
         viewBox="0 0 551 229"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
